bbcc43f22e8a6f744637ec14b6e931b7eee25928
rakhimov <olzhasrakhimov7@gmail.com>
2014-07-02 01:46:48 -0500

    Initial crude tests for fault tree.

--- tests/fault_tree_tests.cc
+++ tests/fault_tree_tests.cc
@@ -1,10 +1,5 @@
 #include "fault_tree_tests.h"
 
-#include <gtest/gtest.h>
-
-#include "error.h"
-#include "risk_analysis.h"
-
 using namespace scram;
 
 // Test Input Processing
@@ -12,16 +7,45 @@ using namespace scram;
 // in fault_tree_input_tests.cc, so this test only concerned with actual changes
 // after processing the input.
 TEST_F(FaultTreeTest, ProcessInput) {
+  std::string tree_input = "./input/fta/correct_tree_input.scramf";
+  FaultTree* fta = new FaultTree("fta-default", false);
+  ASSERT_NO_THROW(fta->ProcessInput(tree_input));
 }
+
 // Test Probability Assignment
 TEST_F(FaultTreeTest, PopulateProbabilities) {
+  std::string tree_input = "./input/fta/correct_tree_input.scramf";
+  std::string prob_input = "./input/fta/correct_prob_input.scramp";
+  FaultTree* fta = new FaultTree("fta-default", false);
+  ASSERT_NO_THROW(fta->ProcessInput(tree_input));
+  ASSERT_NO_THROW(fta->PopulateProbabilities(prob_input));
 }
+
 // Test Graphing Intructions
 TEST_F(FaultTreeTest, GraphingInstructions) {
+  std::string tree_input = "./input/fta/correct_tree_input.scramf";
+  FaultTree* fta = new FaultTree("fta-default", false);
+  ASSERT_NO_THROW(fta->ProcessInput(tree_input));
+  ASSERT_NO_THROW(fta->GraphingInstructions());
 }
+
 // Test Analysis
 TEST_F(FaultTreeTest, Analyze) {
+  std::string tree_input = "./input/fta/correct_tree_input.scramf";
+  std::string prob_input = "./input/fta/correct_prob_input.scramp";
+  FaultTree* fta = new FaultTree("fta-default", false);
+  ASSERT_NO_THROW(fta->ProcessInput(tree_input));
+  ASSERT_NO_THROW(fta->PopulateProbabilities(prob_input));
+  ASSERT_NO_THROW(fta->Analyze());
 }
+
 // Test Reporting capabilities
 TEST_F(FaultTreeTest, Report) {
+  std::string tree_input = "./input/fta/correct_tree_input.scramf";
+  std::string prob_input = "./input/fta/correct_prob_input.scramp";
+  FaultTree* fta = new FaultTree("fta-default", false);
+  ASSERT_NO_THROW(fta->ProcessInput(tree_input));
+  ASSERT_NO_THROW(fta->PopulateProbabilities(prob_input));
+  ASSERT_NO_THROW(fta->Analyze());
+  ASSERT_NO_THROW(fta->Report("/dev/null"));
 }
--- tests/fault_tree_tests.h
+++ tests/fault_tree_tests.h
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include "error.h"
+#include "fault_tree.h"
+
 class FaultTreeTest : public ::testing::Test {
  protected:
   virtual void SetUp() {}
