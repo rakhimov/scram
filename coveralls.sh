df3b61e9112df6d068987dc81497315992bf5ebc
rakhimov <olzhasrakhimov7@gmail.com>
2014-07-04 06:21:18 -0500

    Configurations for Coveralls.

---
+++ coveralls.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Run tests for Coveralls.
+
+cd build
+./bin/scram_unit_tests
+mkdir report
+cd ./src/CMakeFiles/scram.dir/
+# coveralls --dryrun --exclude lib --exclude tests --exclude CMakeFiles --exclude build > /dev/null
+# cp ./*.cc.gcov ../../../report/
+cd ../scramcore.dir/
+coveralls --dryrun --exclude lib --exclude tests --exclude CMakeFiles --exclude build > /dev/null
+cp ./*.cc.gcov ../../../report/
+
+for f in error.h.gcov event.h.gcov fault_tree.h.gcov risk_analysis.h.gcov superset.h.gcov
+do
+  if [ -f f ]; then
+    cp f ../../../report/
+  fi
+done
+
+cd ../../../report/
+coveralls --no-gcov --exclude lib --exclude tests --exclude CMakeFiles --exclude build
