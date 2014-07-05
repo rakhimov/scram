dbcc545db30a17aa8c8e5dae38eb21131e2688cd
rakhimov <olzhasrakhimov7@gmail.com>
2014-07-05 09:58:40 -0500

    Add input tests for transfer sub tree that has a second transferout node.

--- tests/fault_tree_input_tests.cc
+++ tests/fault_tree_input_tests.cc
@@ -92,6 +92,7 @@ TEST(FaultTreeInputTest, IncorrectFTAInputs) {
   incorrect_inputs.push_back("./input/fta/transfer_wrong_type_top.scramf");
   incorrect_inputs.push_back("./input/fta/transfer_wrong_second_node_top.scramf");
   incorrect_inputs.push_back("./input/fta/transfer_primary_second_node_top.scramf");
+  incorrect_inputs.push_back("./input/fta/transfer_second_transferout_top.scramf");
 
   RiskAnalysis* ran;
 
