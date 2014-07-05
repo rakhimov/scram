1e37a76c27608f556bf241b9ddced17d87a361c0
rakhimov <olzhasrakhimov7@gmail.com>
2014-07-05 13:15:37 -0500

    Add graph generation for CI.

--- .run_tests.sh
+++ .run_tests.sh
@@ -6,7 +6,17 @@ if which scram_unit_tests && which scram
 then
   if scram_unit_tests
   then
-    nosetests -w ./tests/
+    if nosetests -w ./tests/
+    then
+      # Check generation and graphing of a complex tree
+      if ./input/tree_generator.py -p 200 -c 5
+      then
+        if scram -g fta_tree.scramf
+        then
+          dot -Tsvg fta_tree.dot -o fta_tree.svg
+        fi
+      fi
+    fi
   fi
 fi
 
