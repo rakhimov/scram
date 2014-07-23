505eecc28a23838b0a44499f261dc99ce83ca7b7
rakhimov <olzhasrakhimov7@gmail.com>
2014-07-23 02:28:18 -0500

    Simplify run tests script.

--- .run_tests.sh
+++ .run_tests.sh
@@ -2,27 +2,11 @@
 
 # Assuming that path contains the built binaries.
 # And this script is called from the root directory.
-if which scram_unit_tests && which scram
-then
-  if scram_unit_tests
-  then
-    if nosetests -w ./tests/
-    then
-      # Check generation and graphing of a complex tree
-      if ./input/tree_generator.py -p 200 -c 5
-      then
-        if scram -g fta_tree.scramf
-        then
-          dot -Tsvg fta_tree.dot -o fta_tree.svg
-        else exit 1
-        fi
-      else exit 1
-      fi
-    else exit 1
-    fi
-  else exit 1
-  fi
-else exit 1
-fi
+which scram_unit_tests && which scram \
+  && scram_unit_tests \
+  && nosetests -w ./tests/ \
+  && ./input/tree_generator.py -p 200 -c 5 \
+  && scram -g fta_tree.scramf \
+  && dot -Tsvg fta_tree.dot -o fta_tree.svg
 
-exit
+exit $?
