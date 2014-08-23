c1a47de9cfb25e62afad8b79d1a6152deeea8cac
rakhimov <olzhasrakhimov7@gmail.com>
2014-08-22 21:54:37 -0500

    Fault Tree generator with XML format and 200 event output. Closes #45.

--- .run_tests.sh
+++ .run_tests.sh
@@ -5,8 +5,8 @@
 which scram_unit_tests && which scram \
   && scram_unit_tests \
   && nosetests -w ./tests/ \
-  && ./input/tree_generator.py -p 200 -c 5 \
-  && scram -g fta_tree.scramf \
-  && dot -Tsvg fta_tree.dot -o fta_tree.svg
+  && ./input/tree_generator.py -p 200 -c 5
+#  && scram -g fta_tree.scramf \
+#  && dot -Tsvg fta_tree.dot -o fta_tree.svg
 
 exit $?
