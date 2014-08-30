1863957e9d4990fd82cb9cc8015faaa356738951
rakhimov <olzhasrakhimov7@gmail.com>
2014-08-30 17:58:48 -0500

    Documentation update.

--- input/performance.sh
+++ input/performance.sh
@@ -7,7 +7,6 @@ echo -e '\n\n'
 echo -e 'Three Motor -c 0'
 scram ThreeMotor/three_motor.xml -c 0 | grep 'Probability'
 
-
 echo -e '\n\n'
 echo -e '200_Event -l 16'
 scram 200_Event/fta_tree.xml -l 16 | head -n 15
