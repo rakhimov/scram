d7b64591ccd6e0ceab1c94a71cfa36e13ca93e23
rakhimov <olzhasrakhimov7@gmail.com>
2014-09-12 21:25:14 -0500

    Recover the python script to run all test inputs for visual check.

--- tests/input/fta/run_inputs.py
+++ tests/input/fta/run_inputs.py
@@ -6,90 +6,67 @@ import subprocess
 def main():
     # Correct corner case inputs
     pass_inputs = [
-            "correct_tree_input.scramf",
-            "different_order.scramf",
-            "doubly_defined_basic.scramf",
-            "inline_comments.scramf",
+            "correct_tree_input.xml",
+            "mixed_definitions.xml",
+            "model_data_mixed_definitions.xml",
+            "trailing_spaces.xml",
             ]
 
     pass_probs = [
-            "correct_prob_input.scramp",
-            "correct_lambda_prob.scramp",
+            "correct_tree_input_with_probs.xml",
             ]
 
     # Wrong input files in the current directory
     bad_inputs = [
-            "basic_top_event.scramf",
-            "conditional_wrong_type_inter.scramf",
-            "conditional_wrong_type_top.scramf",
-            "doubly_defined_intermediate.scramf",
-            "doubly_defined_primary_type.scramf",
-            "doubly_defined_top.scramf",
-            "extra_parameter.scramf",
-            "leaf_intermidiate_event.scramf",
-            "missing_id.scramf",
-            "missing_nodes.scramf",
-            "missing_parameter.scramf",
-            "missing_parent.scramf",
-            "missing_type.scramf",
-            "name_clash_inter.scramf",
-            "name_clash_primary.scramf",
-            "name_clash_top.scramf",
-            "non_existent_parent_inter.scramf",
-            "non_existent_parent_primary.scramf",
-            "nonexistent_file.scramf",
-            "one_arg_err.scramf",
-            "second_closing_brace.scramf",
-            "too_many_args.scramf",
-            "top_event_with_no_child.scramf",
-            "unrecognized_parameter.scramf",
-            "unrecognized_type.scramf",
-            "vote_no_number.scramf",
-            "vote_not_enough_children.scramf",
-            "vote_string.scramf",
+            "../xml_formatting_error.xml",
+            "../schema_fail.xml",
+            "../unsupported_feature.xml",
+            "unsupported_gate.xml",
+            "nonexistent_file.xml",
+            "doubly_defined_gate.xml",
+            "doubly_defined_house.xml",
+            "doubly_defined_basic.xml",
+            "missing_event_definition.xml",
+            "missing_basic_event_definition.xml",
+            "missing_house_event_definition.xml",
+            "missing_gate_definition.xml",
+            "name_clash_basic_gate.xml",
+            "name_clash_house_gate.xml",
+            "name_clash_gate_primary.xml",
+            "name_clash_basic_house.xml",
+            "name_clash_house_basic.xml",
+            "def_clash_basic_gate.xml",
+            "def_clash_house_gate.xml",
+            "def_clash_gate_primary.xml",
+            "def_clash_basic_house.xml",
+            "def_clash_house_basic.xml",
+            "def_name_house_basic.xml",
+            "def_name_basic_house.xml",
+            "atleast_gate.xml",
+            "unordered_structure.xml",
+            "dangling_gate.xml",
+            "non_top_gate.xml",
+            "cyclic_tree.xml",
             ]
-
-    # Wrong probability files in the current directory
-    bad_probs = [
-            "doubly_defined_block.scramp",
-            "doubly_defined_prob.scramp",
-            "doubly_defined_time.scramp",
-            "huge_prob.scramp",
-            "missing_basic_event.scramp",
-            "negative_prob.scramp",
-            "negative_time.scramp",
-            "no_time_given.scramp",
-            "nonexistent_file.scramp",
-            "one_arg_err.scramp",
-            "second_closing_brace.scramp",
-            "string_prob.scramp",
-            "string_time.scramp",
-            "too_many_args.scramp",
-            "unrecognized_block.scramp",
-            ]
-
-    input_correct = "correct_tree_input.scramf"
-    prob_correct = "correct_prob_input.scramp"
-    lambda_correct = "correct_lambda_prob.scramp"
 
     # Run correct inputs
-    print("\nRunning Correct Inputs\n\n")
+    print("\nRUNNING CORRECT INPUTS\n\n")
     for ic in pass_inputs:
-        print("\nRunning : " + ic + "\n")
+        print("\nRUNNING : " + ic + "\n")
         args = ["scram", ic]
         subprocess.call(args)
 
     # Run correct probabilities
-    print("\nRunning Correct Probability Inputs\n\n")
+    print("\nRUNNING CORRECT PROBABILITY INPUTS\n\n")
     for p in pass_probs:
-        print("\nRunning : " + p + "\n")
-        args = ["scram", input_correct, p]
+        print("\nRUNNING : " + p + "\n")
+        args = ["scram", p]
         subprocess.call(args)
 
     # Run incorrect inputs
-    print("\nRunning Incorrect Inputs\n\n")
+    print("\nRUNNING INCORRECT INPUTS\n\n")
     for i in bad_inputs:
-        print("\nRunning : " + i + "\n")
+        print("\nRUNNING : " + i + "\n")
         msg = ""
         args = ["scram", i]
         try:
@@ -97,19 +74,6 @@ def main():
         except:
             msg = sys.exc_info()[0]
         print(msg)
-
-    # Run incorrect probabilities
-    print("\nRunning Incorrect Probabilities\n\n")
-    for p in bad_probs:
-        print("\nRunning : " + p + "\n")
-        msg = ""
-        args = ["scram", input_correct, p]
-        try:
-            subprocess.call(args)
-        except:
-            msg = sys.exc_info()[0]
-        print(msg)
-
 
 if __name__ == "__main__":
     main()
