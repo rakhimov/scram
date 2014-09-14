2d6649d0a2796d977429ec0a7a0badd6f038ae17
rakhimov <olzhasrakhimov7@gmail.com>
2014-09-14 05:58:28 -0500

    Add XML and RNG tests.

--- tests/xml_parser_tests.cc
+++ tests/xml_parser_tests.cc
@@ -2,13 +2,16 @@
 
 #include <iostream>
 #include <fstream>
+
 #include <gtest/gtest.h>
 
 #include "error.h"
+#include "relax_ng_validator.h"
 
 #include "tools.h"
 
 using namespace std;
+using namespace scram;
 
 void XMLParserTests::FillSnippet(std::stringstream& ss) {
   ss << "<" << outer_node_ << ">"
@@ -35,6 +38,19 @@ void XMLParserTests::FillSchema(std::stringstream& ss) {
      << "</grammar>";
 }
 
+void XMLParserTests::FillBadSchema(std::stringstream& ss) {
+  ss << "<grammar xmlns=\"http://relaxng.org/ns/structure/1.0\"" << std::endl
+     << "datatypeLibrary=\"http://www.w3.org/2001/XMLSchema-datatypes\">" << std::endl
+     << "  " << "<start>" << std::endl
+     << "  " << "<element naem =\"" << outer_node_ << "\">" << std::endl
+     << "  " << "  " << "<element name =\"" << inner_node_ << "\">" << std::endl
+     << "  " << "    " << "<text/>" << std::endl
+     << "  " << "  " << "</element>" << std::endl
+     << "  " << "</element>" << std::endl
+     << "  " << "</start>" << std::endl
+     << "</grammar>";
+}
+
 void XMLParserTests::SetUp() {
   inner_node_ = "inside";
   outer_node_ = "outside";
@@ -42,6 +58,29 @@ void XMLParserTests::SetUp() {
 }
 
 void XMLParserTests::TearDown() {}
+
+// This is an indirect test of the validator.
+TEST_F(XMLParserTests, RelaxNGValidator) {
+  std::stringstream snippet("");
+  FillSnippet(snippet);
+  stringstream schema("");
+  FillSchema(schema);
+
+  scram::XMLParser parser;
+  EXPECT_NO_THROW(parser.Init(snippet));
+
+  RelaxNGValidator validator;
+  xmlpp::Document* doc = 0;
+  EXPECT_NO_THROW(validator.parse_memory(schema.str()));
+  EXPECT_THROW(validator.Validate(doc), ValidationError);
+
+  doc = parser.Document();
+  validator = RelaxNGValidator();
+  EXPECT_THROW(validator.Validate(doc), ValidationError);  // Not initialized.
+
+  EXPECT_NO_THROW(validator.parse_memory(schema.str()));
+  EXPECT_NO_THROW(validator.Validate(doc));  // Initialized.
+}
 
 TEST_F(XMLParserTests, WithoutSchema) {
   std::stringstream snippet("");
@@ -58,6 +97,16 @@ TEST_F(XMLParserTests, WithSchema) {
   scram::XMLParser parser;
   EXPECT_NO_THROW(parser.Init(snippet));
   EXPECT_NO_THROW(parser.Validate(schema));
+}
+
+TEST_F(XMLParserTests, WithBadSchema) {
+  std::stringstream snippet("");
+  FillSnippet(snippet);
+  stringstream schema("");
+  FillBadSchema(schema);
+  scram::XMLParser parser;
+  EXPECT_NO_THROW(parser.Init(snippet));
+  EXPECT_THROW(parser.Validate(schema), ValidationError);
 }
 
 TEST_F(XMLParserTests, WithError) {
--- tests/xml_parser_tests.h
+++ tests/xml_parser_tests.h
@@ -17,6 +17,7 @@ class XMLParserTests : public ::testing::Test {
   void FillSnippet(std::stringstream &ss);
   void FillBadSnippet(std::stringstream &ss);
   void FillSchema(std::stringstream &ss);
+  void FillBadSchema(std::stringstream &ss);
   std::string outer_node_;
   std::string inner_node_;
   std::string inner_content_;
