1b64c093681798689c2bd5f5c5338c415abadb48
rakhimov <olzhasrakhimov7@gmail.com>
2014-09-26 12:23:38 -0500

    Add new performance information into the log and script.

--- input/performance.sh
+++ input/performance.sh
@@ -18,3 +18,15 @@ scram 200_Event/fta_tree.xml -l 17 | head -n 15
 echo -e '\n\n'
 echo -e 'Baobab -l 6'
 scram Baobab/baobab1.xml -l 6 | head -n 20
+
+echo -e '\n\n'
+echo -e 'Baobab -l 7'
+scram Baobab/baobab1.xml -l 7 | head -n 20
+
+echo -e '\n\n'
+echo -e 'CEA9601 -l 4'
+scram CEA9601/CEA9601.xml -l 4 | head -n 20
+
+echo -e '\n\n'
+echo -e 'CEA9601 -l 5'
+scram CEA9601/CEA9601.xml -l 5 | head -n 20
