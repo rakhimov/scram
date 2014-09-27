08a6e7d5f70a60b23b1f9397488baf73f5c61303
rakhimov <olzhasrakhimov7@gmail.com>
2014-09-26 20:08:32 -0500

    Add Element class and Attribute struct.

---
+++ tests/element_tests.cc
@@ -0,0 +1,28 @@
+#include "element.h"
+
+#include <gtest/gtest.h>
+
+#include "error.h"
+
+using namespace scram;
+
+TEST(ElementTest, Label) {
+  Element el = Element();
+  EXPECT_EQ("", el.label());
+  EXPECT_THROW(el.label(""), ValueError);
+  ASSERT_NO_THROW(el.label("label"));
+  EXPECT_THROW(el.label("new_label"), ValueError);
+}
+
+TEST(ElementTest, Attribute) {
+  Element el = Element();
+  Attribute attr = Attribute();
+  attr.name = "impact";
+  attr.value = "0.1";
+  attr.type = "float";
+  EXPECT_THROW(el.GetAttribute(attr.name), ValueError);
+  ASSERT_NO_THROW(el.AddAttribute(attr));
+  EXPECT_THROW(el.AddAttribute(attr), ValueError);
+  ASSERT_TRUE(el.HasAttribute(attr.name));
+  ASSERT_NO_THROW(el.GetAttribute(attr.name));
+}
