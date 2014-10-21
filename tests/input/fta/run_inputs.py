0c32ca9195164d81af9fdae9e3ffffda2c3ec74c
rakhimov <olzhasrakhimov7@gmail.com>
2014-10-20 22:51:17 -0500

    Add new test inputs into python script.

--- tests/input/fta/run_inputs.py
+++ tests/input/fta/run_inputs.py
@@ -10,6 +10,11 @@ def main():
             "mixed_definitions.xml",
             "model_data_mixed_definitions.xml",
             "trailing_spaces.xml",
+            "two_trees.xml",
+            "labels_and_attributes.xml",
+            "orphan_primary_event.xml",
+            "correct_expressions.xml",
+            "flavored_types.xml",
             ]
 
     pass_probs = [
@@ -22,19 +27,25 @@ def main():
             "../schema_fail.xml",
             "../unsupported_feature.xml",
             "unsupported_gate.xml",
+            "unsupported_expression.xml",
             "nonexistent_file.xml",
             "doubly_defined_gate.xml",
             "doubly_defined_house.xml",
             "doubly_defined_basic.xml",
+            "doubly_defined_parameter.xml",
             "missing_event_definition.xml",
             "missing_basic_event_definition.xml",
             "missing_house_event_definition.xml",
+            "missing_expression.xml",
+            "missing_bool_constant.xml",
+            "missing_parameter.xml",
             "missing_gate_definition.xml",
             "name_clash_basic_gate.xml",
             "name_clash_house_gate.xml",
             "name_clash_gate_primary.xml",
             "name_clash_basic_house.xml",
             "name_clash_house_basic.xml",
+            "name_clash_two_trees.xml",
             "def_clash_basic_gate.xml",
             "def_clash_house_gate.xml",
             "def_clash_gate_primary.xml",
@@ -47,24 +58,27 @@ def main():
             "dangling_gate.xml",
             "non_top_gate.xml",
             "cyclic_tree.xml",
+            "cyclic_parameter.xml",
+            "invalid_probability.xml",
+            "invalid_expression.xml",
             ]
 
     # Run correct inputs
-    print("\nRUNNING CORRECT INPUTS\n\n")
+    print("\n\nRUNNING CORRECT INPUTS\n")
     for ic in pass_inputs:
         print("\nRUNNING : " + ic + "\n")
         args = ["scram", ic]
         subprocess.call(args)
 
     # Run correct probabilities
-    print("\nRUNNING CORRECT PROBABILITY INPUTS\n\n")
+    print("\n\nRUNNING CORRECT PROBABILITY INPUTS\n")
     for p in pass_probs:
         print("\nRUNNING : " + p + "\n")
         args = ["scram", p]
         subprocess.call(args)
 
     # Run incorrect inputs
-    print("\nRUNNING INCORRECT INPUTS\n\n")
+    print("\n\nRUNNING INCORRECT INPUTS\n")
     for i in bad_inputs:
         print("\nRUNNING : " + i + "\n")
         msg = ""
