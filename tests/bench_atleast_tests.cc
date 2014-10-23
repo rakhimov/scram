26fb877eff94d3d8e8c32156dd8b8f649f592b72
rakhimov <olzhasrakhimov7@gmail.com>
2014-10-22 20:50:19 -0500

    Add simple test for Atleast gate.

---
+++ tests/bench_atleast_tests.cc
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+
+#include "risk_analysis_tests.h"
+
+// Simple verification tests for Atleast gate fault tree.
+// Test Minimal cut sets and total probabilty.
+TEST_F(RiskAnalysisTest, ATLEAST) {
+  std::string tree_input = "./share/scram/input/benchmark/atleast.xml";
+  std::string A = "a";  // 0.1
+  std::string B = "b";  // 0.2
+  std::string C = "c";  // 0.3
+  std::set<std::string> cut_set;
+  std::set< std::set<std::string> > mcs;  // For expected min cut sets.
+
+  ASSERT_NO_THROW(ran->ProcessInput(tree_input));
+  ASSERT_NO_THROW(ran->Analyze());
+  ASSERT_NO_THROW(ran->Report("/dev/null"));
+  EXPECT_DOUBLE_EQ(0.098, p_total());  // Total prob check.
+  // Minimal cut set check.
+  cut_set.insert(A);
+  cut_set.insert(C);
+  mcs.insert(cut_set);
+  cut_set.clear();
+  cut_set.insert(A);
+  cut_set.insert(B);
+  mcs.insert(cut_set);
+  cut_set.clear();
+  cut_set.insert(B);
+  cut_set.insert(C);
+  mcs.insert(cut_set);
+  EXPECT_EQ(3, min_cut_sets().size());
+  EXPECT_EQ(mcs, min_cut_sets());
+}
