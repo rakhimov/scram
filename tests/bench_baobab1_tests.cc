ed865daaf4a9488916711ac6de2b002b99a5d4bb
rakhimov <olzhasrakhimov7@gmail.com>
2014-11-16 12:42:15 -0600

    Add Baobab 1 benchmark test from XFTA.

---
+++ tests/bench_baobab1_tests.cc
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include "risk_analysis_tests.h"
+
+// Benchmark Tests for Baobab 1 fault tree from XFTA.
+// Test Minimal cut sets.
+TEST_F(RiskAnalysisTest, Baobab_1_Test) {
+  std::string tree_input = "./share/scram/input/benchmark/baobab1.xml";
+  ran->AddSettings(settings.limit_order(6));
+  ASSERT_NO_THROW(ran->ProcessInput(tree_input));
+  ASSERT_NO_THROW(ran->Analyze());
+  ASSERT_NO_THROW(ran->Report("/dev/null"));
+  // Minimal cut set check.
+  EXPECT_EQ(2684, min_cut_sets().size());
+  std::vector<int> distr(7, 0);
+  distr[2] = 1;
+  distr[3] = 1;
+  distr[4] = 70;
+  distr[5] = 400;
+  distr[6] = 2212;
+  EXPECT_EQ(distr, McsDistribution());
+}
