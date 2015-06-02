d9c32feca6063702f5bd0fcb70bfd4e3f628d020
rakhimov <ol.rakhimov@gmail.com>
2015-06-02 00:20:30 -0700

    Add 90% test coverage for the shorthand-to-XML converter.
    Covered cases: nested formula, formatting, fault tree errors, cycles,
    precedence.
    
    Parentheses cases do not work in the implementation.

--- scripts/shorthand_to_xml.py
+++ scripts/shorthand_to_xml.py
@@ -33,19 +33,19 @@ from __future__ import print_function
 from collections import deque
 import os
 import re
-from tempfile import NamedTemporaryFile
 
 import argparse as ap
-from nose.tools import assert_raises
 
 
 class ParsingError(Exception):
     """General parsing errors."""
     pass
 
+
 class FormatError(Exception):
     """Common errors in writing the shorthand format."""
     pass
+
 
 class FaultTreeError(Exception):
     """Indication of problems in the fault tree."""
@@ -58,6 +58,7 @@ class Node(object):
     Attributes:
         name: A specific name that identifies this node.
     """
+
     def __init__(self, name):
         self.name = name
         self.__parents = set()
@@ -83,6 +84,7 @@ class BasicEvent(Node):
     Attributes:
         prob: Probability of failure of this basic event.
     """
+
     def __init__(self, name, prob):
         super(BasicEvent, self).__init__(name)
         self.prob = prob
@@ -94,6 +96,7 @@ class HouseEvent(Node):
     Attributes:
         state: State of the house event ("true" or "false").
     """
+
     def __init__(self, name, state):
         super(HouseEvent, self).__init__(name)
         self.state = state
@@ -106,6 +109,7 @@ class Gate(Node):
         mark: Marking for various algorithms like toposort.
         formula: The formula of this gate.
     """
+
     def __init__(self, name, formula):
         super(Gate, self).__init__(name)
         self.mark = None  # marking for various algorithms
@@ -125,6 +129,7 @@ class Formula(object):
         h_arguments: arguments that are house events.
         u_arguments: arguments that are undefined from the input.
     """
+
     def __init__(self, operator=None, k_num=None):
         self.operator = operator
         self.k_num = k_num
@@ -153,6 +158,7 @@ class FaultTree(object):
         top_gates: The top gates of a fault tree. Single one is the default.
         multi_top: A flag to indicate to allow multiple top gates.
     """
+
     def __init__(self, name=None, multi_top=False):
         self.name = name
         self.gates = {}
@@ -393,9 +399,9 @@ def parse_input_file(input_file, multi_top=False):
     # Optional parentheses for gates
     paren_re = re.compile(r"\s*\((.*)\)\s*$")
     # AND gate identification
-    and_re = re.compile(r"(\s*[^\(\)]+(\s*&\s*[^\(\)]+\s*)+)$")
+    and_re = re.compile(r"(\s*.+(\s*&\s*.+\s*)+)$")
     # OR gate identification
-    or_re = re.compile(r"(\s*[^\(\)]+(\s*\|\s*[^\(\)]+\s*)+)$")
+    or_re = re.compile(r"(\s*.+(\s*\|\s*.+\s*)+)$")
     # Combination gate identification
     comb_children = r"\[(\s*.+(\s*,\s*.+\s*){2,})\]"
     comb_re = re.compile(r"@\(([2-9])\s*,\s*" + comb_children + r"\s*\)\s*$")
@@ -719,6 +725,40 @@ if __name__ == "__main__":
         print("Error in the fault tree:")
         print(fault_tree_error)
 
+
+from tempfile import NamedTemporaryFile
+from unittest import TestCase
+
+from lxml import etree
+from nose.tools import assert_raises, assert_is_not_none, assert_equal, \
+        assert_true
+
+def test_correct():
+    """Tests the valid overall process."""
+    tmp = NamedTemporaryFile()
+    tmp.write("ValidFaultTree\n\n")
+    tmp.write("root := g1 | g2 | g3 | g4 | e1\n")
+    tmp.write("g1 := e2 | g3 & g5\n")
+    tmp.write("g2 := h1 & g6\n")
+    tmp.write("g3 := (g6 ^ e2)\n")
+    tmp.write("g4 := @(2, [g5, e3, e4])\n")
+    tmp.write("g5 := ~e3\n")
+    tmp.write("g6 := ((e3 | e4))\n\n")
+    tmp.write("p(e1) = 0.1\n")
+    tmp.write("p(e2) = 0.2\n")
+    tmp.write("p(e3) = 0.3\n")
+    tmp.write("s(h1) = true\n")
+    tmp.write("s(h2) = false\n")
+    tmp.flush()
+    fault_tree = parse_input_file(tmp.name)
+    assert_is_not_none(fault_tree)
+    out = NamedTemporaryFile()
+    write_to_xml_file(fault_tree, out.name)
+    relaxng_doc = etree.parse("../share/input.rng")
+    relaxng = etree.RelaxNG(relaxng_doc)
+    doc = etree.parse(out)
+    assert_true(relaxng.validate(doc))
+
 def test_ft_name_redefinition():
     """Tests the redefinition of the fault tree name."""
     tmp = NamedTemporaryFile()
@@ -737,6 +777,46 @@ def test_ncname_ft():
     tmp.write("NOT-NCNAME-Fault-tree.\n")
     tmp.flush()
     assert_raises(ParsingError, parse_input_file, tmp.name)
+
+def test_no_ft_name():
+    """Tests the case where no fault tree name is provided."""
+    tmp = NamedTemporaryFile()
+    tmp.write("g1 := g2 & e1\n")
+    tmp.write("g2 := h1 & e1\n")
+    tmp.flush()
+    assert_raises(FormatError, parse_input_file, tmp.name)
+
+def test_illegal_format():
+    """Test Arithmetic operators."""
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 + e1\n")
+    tmp.flush()
+    assert_raises(ParsingError, parse_input_file, tmp.name)
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 * e1\n")
+    tmp.flush()
+    assert_raises(ParsingError, parse_input_file, tmp.name)
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := -e1\n")
+    tmp.flush()
+    assert_raises(ParsingError, parse_input_file, tmp.name)
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 / e1\n")
+    tmp.flush()
+    assert_raises(ParsingError, parse_input_file, tmp.name)
+
+def test_repeated_argument():
+    """Tests the formula with a repeated argument."""
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 & e1\n")
+    tmp.write("g2 := E1 & e1\n")  # repeated argument with uppercase
+    tmp.flush()
+    assert_raises(FaultTreeError, parse_input_file, tmp.name)
 
 def test_parenthesis_count():
     """Tests cases with missing parenthesis."""
@@ -758,3 +838,197 @@ def test_combination_gate_children():
     tmp.write("g1 := @(4, [a, b, c])")  # K > N
     tmp.flush()
     assert_raises(FaultTreeError, parse_input_file, tmp.name)
+
+def test_no_top_event():
+    """Detection of cases without top gate definitions.
+
+    Note that this also means that there is a cycle that includes the root.
+    """
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 & e1\n")
+    tmp.write("g2 := g1 & e1\n")
+    tmp.flush()
+    assert_raises(FaultTreeError, parse_input_file, tmp.name)
+
+def test_multi_top():
+    """Multiple root nodes without the flag causes a problem by default."""
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := e2 & e1\n")
+    tmp.write("g2 := h1 & e1\n")
+    tmp.flush()
+    assert_raises(FaultTreeError, parse_input_file, tmp.name)
+    assert_is_not_none(parse_input_file(tmp.name, True))  # with the flag
+
+def test_redefinition():
+    """Tests name collision detection of nodes."""
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 & e1\n")
+    tmp.write("g2 := h1 & e1\n")
+    tmp.write("g2 := e2 & e1\n")  # redefining a node
+    tmp.flush()
+    assert_raises(FaultTreeError, parse_input_file, tmp.name)
+
+def test_orphan_nodes():
+    """Tests cases with orphan house and basic event nodes."""
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 & e1\n")
+    tmp.write("g2 := h1 & e1\n")
+    tmp.write("p(e1) = 0.5\n")
+    tmp.write("s(h1) = false\n")
+    tmp.flush()
+    assert_is_not_none(parse_input_file(tmp.name))
+    tmp.write("p(e2) = 0.1\n")  # orphan basic event
+    tmp.flush()
+    assert_is_not_none(parse_input_file(tmp.name))
+    tmp.write("s(h2) = true\n")  # orphan house event
+    tmp.flush()
+    assert_is_not_none(parse_input_file(tmp.name))
+
+def test_cycle_detection():
+    """Tests cycles in the fault tree"""
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := g2 & e1\n")
+    tmp.write("g2 := g3 & e1\n")
+    tmp.write("g3 := g2 & e1\n")  # cycle
+    tmp.flush()
+    assert_raises(FaultTreeError, parse_input_file, tmp.name)
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := u1 | g2 & e1\n")
+    tmp.write("g2 := u2 | g3 & e1\n")  # nested formula cycle
+    tmp.write("g3 := u3 | g2 & e1\n")  # cycle
+    tmp.flush()
+    assert_raises(FaultTreeError, parse_input_file, tmp.name)
+
+def test_detached_gates():
+    """Some cycles may get detached from the original fault tree."""
+    tmp = NamedTemporaryFile()
+    tmp.write("FT\n")
+    tmp.write("g1 := e2 & e1\n")
+    tmp.write("g2 := g3 & e1\n")  # detached gate
+    tmp.write("g3 := g2 & e1\n")  # cycle
+    tmp.flush()
+    assert_raises(FaultTreeError, parse_input_file, tmp.name)
+
+class OperatorPrecedenceTestCase(TestCase):
+    """Logical operator precedence tests."""
+
+    def setUp(self):
+        self.tmp = NamedTemporaryFile()
+        self.tmp.write("FT\n")
+
+    def two_operators(self, op_one, op_two, num_args=3):
+        """Common operations for two operator tests.
+
+        The requirement for the input is to have g1 gate with a nested formula
+        with num_args arguments. Only the first argument is associated with
+        the first operator, and the rest of arguments are associated with the
+        second operator. The arguments must be named e1, e2, e3, ...
+
+        Args:
+            op_one: The first operator of higher precedence.
+            op_two: The second operator of lower precedence.
+            num_args: The number of arguments in the nested formula.
+        """
+        args = []
+        for i in range(1, num_args + 1):
+            args.append("e%d" % i)
+
+        fault_tree = parse_input_file(self.tmp.name)
+        assert_equal(len(fault_tree.gates), 1)
+        gate = fault_tree.gates["g1"].formula
+        assert_equal(gate.num_arguments(), 2)
+        assert_equal(gate.operator, op_one)
+        assert_equal(gate.node_arguments, args[:1])
+        assert_equal(len(gate.f_arguments), 1)
+        child_f = gate.f_arguments[0]
+        assert_equal(child_f.num_arguments(), num_args - 1)
+        assert_equal(child_f.operator, op_two)
+        assert_equal(child_f.node_arguments, args[1:])
+
+    def test_or_xor(self):
+        """Formula with OR and XOR operators."""
+        self.tmp.write("g1 := e1 | e2 ^ e3\n")
+        self.tmp.flush()
+        self.two_operators("or", "xor")
+
+    def test_or_and(self):
+        """Formula with OR and AND operators."""
+        self.tmp.write("g1 := e1 | e2 & e3\n")
+        self.tmp.flush()
+        self.two_operators("or", "and")
+
+    def test_or_atleast(self):
+        """Formula with OR and K/N operators."""
+        self.tmp.write("g1 := e1 | @(2, [e2, e3, e4])\n")
+        self.tmp.flush()
+        self.two_operators("or", "atleast", 4)
+
+    def test_or_not(self):
+        """Formula with OR and NOT operators."""
+        self.tmp.write("g1 := e1 | ~e2\n")
+        self.tmp.flush()
+        self.two_operators("or", "not", 2)
+
+    def test_xor_xor(self):
+        """Formula with XOR and XOR operators.
+
+        Note that this is a special case because most analysis restricts
+        XOR operator to two arguments, so nested formula of XOR operators
+        must be created.
+        """
+        self.tmp.write("g1 := e1 ^ e2 ^ e3\n")
+        self.tmp.flush()
+        self.two_operators("xor", "xor")
+
+    def test_xor_and(self):
+        """Formula with XOR and AND operators."""
+        self.tmp.write("g1 := e1 ^ e2 & e3\n")
+        self.tmp.flush()
+        self.two_operators("xor", "and")
+
+    def test_xor_atleast(self):
+        """Formula with XOR and K/N operators."""
+        self.tmp.write("g1 := e1 ^ @(2, [e2, e3, e4])\n")
+        self.tmp.flush()
+        self.two_operators("xor", "atleast", 4)
+
+    def test_xor_not(self):
+        """Formula with XOR and NOT operators."""
+        self.tmp.write("g1 := e1 ^ ~e2\n")
+        self.tmp.flush()
+        self.two_operators("xor", "not", 2)
+
+    def test_and_atleast(self):
+        """Formula with AND and K/N operators."""
+        self.tmp.write("g1 := e1 & @(2, [e2, e3, e4])\n")
+        self.tmp.flush()
+        self.two_operators("and", "atleast", 4)
+
+    def test_and_not(self):
+        """Formula with AND and NOT operators."""
+        self.tmp.write("g1 := e1 & ~e2\n")
+        self.tmp.flush()
+        self.two_operators("and", "not", 2)
+
+class ParenthesesTestCase(TestCase):
+    """Application of parentheses tests."""
+
+    def setUp(self):
+        self.tmp = NamedTemporaryFile()
+        self.tmp.write("FT\n")
+
+    def test_atleast_not(self):
+        """Formula with K/N containing NOT operator."""
+        self.tmp.write("g1 := @(2, [~e1, e2, e3])\n")
+        self.tmp.flush()
+
+    def test_not_atleast(self):
+        """Formula with negation of K/N operator."""
+        self.tmp.write("g1 := ~@(2, [e1, e2, e3])\n")
+        self.tmp.flush()
