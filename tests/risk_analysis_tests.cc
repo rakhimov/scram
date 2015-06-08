3703a7968cf46c4f88facff6c84315dd27d6dd5a
rakhimov <ol.rakhimov@gmail.com>
2015-06-08 03:37:23 -0700

    Add tests for reporting orphan events and unused parameters.

--- tests/risk_analysis_tests.cc
+++ tests/risk_analysis_tests.cc
@@ -526,6 +526,50 @@ TEST_F(RiskAnalysisTest, ReportAll) {
   ASSERT_NO_THROW(parser->Validate(schema));
 }
 
+// Reporting of orphan primary events.
+TEST_F(RiskAnalysisTest, ReportOrphanPrimaryEvents) {
+  std::string tree_input =
+      "./share/scram/input/fta/orphan_primary_event.xml";
+
+  std::stringstream schema;
+  std::string schema_path = Env::report_schema();
+  std::ifstream schema_stream(schema_path.c_str());
+  schema << schema_stream.rdbuf();
+  schema_stream.close();
+
+  ASSERT_NO_THROW(ran->ProcessInput(tree_input));
+  ASSERT_NO_THROW(ran->Analyze());
+
+  std::stringstream output;
+  ASSERT_NO_THROW(ran->Report(output));
+
+  boost::shared_ptr<XMLParser> parser(new XMLParser());
+  ASSERT_NO_THROW(parser->Init(output));
+  ASSERT_NO_THROW(parser->Validate(schema));
+}
+
+// Reporting of unused parameters.
+TEST_F(RiskAnalysisTest, ReportUnusedParameters) {
+  std::string tree_input =
+      "./share/scram/input/fta/unused_parameter.xml";
+
+  std::stringstream schema;
+  std::string schema_path = Env::report_schema();
+  std::ifstream schema_stream(schema_path.c_str());
+  schema << schema_stream.rdbuf();
+  schema_stream.close();
+
+  ASSERT_NO_THROW(ran->ProcessInput(tree_input));
+  ASSERT_NO_THROW(ran->Analyze());
+
+  std::stringstream output;
+  ASSERT_NO_THROW(ran->Report(output));
+
+  boost::shared_ptr<XMLParser> parser(new XMLParser());
+  ASSERT_NO_THROW(parser->Init(output));
+  ASSERT_NO_THROW(parser->Validate(schema));
+}
+
 // NAND and NOR as a child cases.
 TEST_F(RiskAnalysisTest, ChildNandNorGates) {
   std::string tree_input = "./share/scram/input/fta/children_nand_nor.xml";
