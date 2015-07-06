6ac73affa50d54ef93ab7be8dfda6ab759fa8b0d
rakhimov <ol.rakhimov@gmail.com>
2015-07-05 23:02:52 -0700

    Fix resource leak in element tests
    This leak was reported by Coverity.

--- tests/element_tests.cc
+++ tests/element_tests.cc
@@ -9,24 +9,22 @@ using namespace scram;
 class TestElement : public Element {};
 
 TEST(ElementTest, Label) {
-  Element* el = new TestElement();
-  EXPECT_EQ("", el->label());
-  EXPECT_THROW(el->label(""), LogicError);
-  ASSERT_NO_THROW(el->label("label"));
-  EXPECT_THROW(el->label("new_label"), LogicError);
-  delete el;
+  TestElement el = TestElement();
+  EXPECT_EQ("", el.label());
+  EXPECT_THROW(el.label(""), LogicError);
+  ASSERT_NO_THROW(el.label("label"));
+  EXPECT_THROW(el.label("new_label"), LogicError);
 }
 
 TEST(ElementTest, Attribute) {
-  Element* el = new TestElement();
+  TestElement el = TestElement();
   Attribute attr = Attribute();
   attr.name = "impact";
   attr.value = "0.1";
   attr.type = "float";
-  EXPECT_THROW(el->GetAttribute(attr.name), LogicError);
-  ASSERT_NO_THROW(el->AddAttribute(attr));
-  EXPECT_THROW(el->AddAttribute(attr), LogicError);
-  ASSERT_TRUE(el->HasAttribute(attr.name));
-  ASSERT_NO_THROW(el->GetAttribute(attr.name));
-  delete el;
+  EXPECT_THROW(el.GetAttribute(attr.name), LogicError);
+  ASSERT_NO_THROW(el.AddAttribute(attr));
+  EXPECT_THROW(el.AddAttribute(attr), LogicError);
+  ASSERT_TRUE(el.HasAttribute(attr.name));
+  ASSERT_NO_THROW(el.GetAttribute(attr.name));
 }
