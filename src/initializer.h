4fd0846e563bc721a0f1ccfb4fae4d893a074377
rakhimov <ol.rakhimov@gmail.com>
2015-07-10 17:06:19 -0700

    Remove forward declared enum in Initialer
    This is an error not caught due to inclusion of a header with the
    declaration of the enum.

--- src/initializer.h
+++ src/initializer.h
@@ -25,7 +25,6 @@ class CcfGroup;
 class Expression;
 class Formula;
 class XMLParser;
-enum Units;
 
 /// @class Initializer
 /// This class operates on input files to initialize analysis constructs like
