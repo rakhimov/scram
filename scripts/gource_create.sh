4c888cac0c0d8c1f9bc8012b8662000067142338
rakhimov <ol.rakhimov@gmail.com>
2015-07-20 19:42:55 -0700

    Remove the Gourse script
    This script is not specific to the project.

--- scripts/gource_create.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Run Gource to create video from the Git history of a project.
-# The script needs Gource and avconv.
-
-gource -s 0.25 -a 0.5 -1280x720 -o - \
-  | avconv -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 \
-  -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.mp4
