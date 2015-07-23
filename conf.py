3173b4136b0b289343d98c1f09269eb788020d26
rakhimov <ol.rakhimov@gmail.com>
2015-07-23 09:34:14 -0700

    Make Sphinx exclude README.rst and publish/
    These files are for publishers.

--- conf.py
+++ conf.py
@@ -74,7 +74,7 @@ release = '0.8.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['README.rst', 'publish/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
