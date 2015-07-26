648172b40f6e9b83a3b29fcecc6277766ed052f0
rakhimov <ol.rakhimov@gmail.com>
2015-07-26 15:29:01 -0700

    Attach license boilerplate to the source files

--- src/relax_ng_validator.h
+++ src/relax_ng_validator.h
@@ -1,15 +1,24 @@
+/*
+ * Copyright (C) 2000 Ari Johnson
+ * Copyright (C) 2002-2004 The libxml dev team
+ * Copyright (C) 2014-2015 Olzhas Rakhimov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /// @file relax_ng_validator.h
 /// RelaxNG Validator.
-/* relaxngvalidator.h
- * this class is agented off of the schemavalidator in libxml++
- * here is their license statement:
- *
- * libxml++ and this file are copyright (C) 2000 by Ari Johnson,
- * (C) 2002-2004 by the libxml dev team and
- * are covered by the GNU Lesser General Public License, which should be
- * included with libxml++ as the file COPYING.
- */
-
+/// This class is agented off of the schemavalidator in libxml++.
 #ifndef SCRAM_SRC_RELAX_NG_VALIDATOR_H_
 #define SCRAM_SRC_RELAX_NG_VALIDATOR_H_
 
