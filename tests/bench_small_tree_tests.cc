68cd2b05349034d8c6674e84bdbcd12e22d74242
rakhimov <ol.rakhimov@gmail.com>
2015-08-20 18:22:50 -0700

    Simplify and adjust SmallTree benchmark test code
    Tests are made a bit more precise.

--- tests/bench_small_tree_tests.cc
+++ tests/bench_small_tree_tests.cc
@@ -26,27 +26,15 @@ namespace test {
 // This benchmark is for uncertainty analysis.
 TEST_F(RiskAnalysisTest, SmallTree) {
   std::string tree_input = "./share/scram/input/SmallTree/SmallTree.xml";
-  std::string e1 = "e1";
-  std::string e2 = "e2";
-  std::string e3 = "e3";
-  std::string e4 = "e4";
-  std::set<std::string> cut_set;
-  std::set< std::set<std::string> > mcs;  // For expected min cut sets.
   settings.uncertainty_analysis(true);
+  settings.num_trials(10000);
   ASSERT_NO_THROW(ProcessInputFile(tree_input));
   ASSERT_NO_THROW(ran->Analyze());
-  // Minimal cut set check.
-  cut_set.insert(e1);
-  cut_set.insert(e2);
-  mcs.insert(cut_set);
-  cut_set.clear();
-  cut_set.insert(e3);
-  cut_set.insert(e4);
-  mcs.insert(cut_set);
+  std::set<std::set<std::string>> mcs = {{"e1", "e2"}, {"e3", "e4"}};
   EXPECT_EQ(2, min_cut_sets().size());
   EXPECT_EQ(mcs, min_cut_sets());
-  EXPECT_NEAR(0.02495, mean(), 5e-3);
-  EXPECT_NEAR(0.023625, sigma(), 5e-3);
+  EXPECT_NEAR(0.02565, mean(), 1e-3);
+  EXPECT_NEAR(0.022765, sigma(), 2e-3);
 }
 
 }  // namespace test
