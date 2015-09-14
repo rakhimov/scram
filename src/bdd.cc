a3c2bac632d0b2855ee96d627371c829e8a6dd53
rakhimov <ol.rakhimov@gmail.com>
2015-09-13 18:41:04 -0700

    Add processing of Boolean Graph Gate into BDD ITE
    Only very simple gates are considered.
    The graph is also assumed to be coherent and normal.

--- src/bdd.cc
+++ src/bdd.cc
@@ -20,19 +20,27 @@
 
 #include "bdd.h"
 
+#include <algorithm>
+
 namespace scram {
 
 Vertex::~Vertex() {}  // Empty body for pure virtual destructor.
 
 Terminal::Terminal(bool value) : value_(value) {}
 
-NonTerminal::NonTerminal(int index) : index_(index) {}
+Ite::Ite(int index) : index_(index) {}
 
-Bdd::Bdd(BooleanGraph* fault_tree) : fault_tree_(fault_tree) {}
+Bdd::Bdd(BooleanGraph* fault_tree)
+    : fault_tree_(fault_tree),
+      kOne_(std::make_shared<Terminal>(true)),
+      kZero_(std::make_shared<Terminal>(false)) {}
 
 void Bdd::Analyze() noexcept {
+  assert(fault_tree_->coherent());
   fault_tree_->ClearOptiValues();
   Bdd::TopologicalOrder(fault_tree_->root(), 0);
+  fault_tree_->ClearGateMarks();
+  Bdd::ConvertGates(fault_tree_->root());
 }
 
 int Bdd::TopologicalOrder(const IGatePtr& root, int order) noexcept {
@@ -44,8 +52,64 @@ int Bdd::TopologicalOrder(const IGatePtr& root, int order) noexcept {
   for (const std::pair<int, VariablePtr>& arg : root->variable_args()) {
     if (!arg.second->opti_value()) arg.second->opti_value(++order);
   }
+  assert(root->constant_args().empty());
   root->opti_value(++order);
   return order;
+}
+
+void Bdd::ConvertGates(const IGatePtr& root) noexcept {
+  if (root->mark()) return;
+  root->mark(true);
+  Bdd::ProcessGate(root);
+  for (const std::pair<int, IGatePtr>& arg : root->gate_args()) {
+    Bdd::ConvertGates(arg.second);
+  }
+}
+
+void Bdd::ProcessGate(const IGatePtr& gate) noexcept {
+  std::vector<std::pair<int, NodePtr>> args;
+  args.insert(args.end(), gate->gate_args().begin(), gate->gate_args().end());
+  args.insert(args.end(), gate->variable_args().begin(),
+              gate->variable_args().end());
+  std::sort(args.begin(), args.end(),
+            [](const std::pair<int, NodePtr>& lhs,
+               const std::pair<int, NodePtr>& rhs) {
+              return lhs.second->opti_value() > rhs.second->opti_value();
+            });
+  gates_.emplace(gate->index(), Bdd::IfThenElse(gate->type(), args, 0));
+}
+
+std::shared_ptr<Ite> Bdd::IfThenElse(
+    Operator type,
+    const std::vector<std::pair<int, NodePtr>>& args,
+    int pos) noexcept {
+  assert(pos >= 0);
+  assert(pos < args.size());
+  const auto& arg = args[pos];
+  ++pos;  // Next argument for subgraphs.
+  assert(arg.first > 0);
+  ItePtr ite(new Ite(arg.second->index()));
+  switch (type) {
+    case kOrGate:
+      ite->high(kOne_);
+      if (pos == args.size()) {
+        ite->low(kZero_);
+      } else {
+        ite->low(Bdd::IfThenElse(type, args, pos));
+      }
+      break;
+    case kAndGate:
+      if (pos == args.size()) {
+        ite->high(kOne_);
+      } else {
+        ite->high(Bdd::IfThenElse(type, args, pos));
+      }
+      ite->low(kZero_);
+      break;
+    default:
+      assert(false);
+  }
+  return ite;
 }
 
 }  // namespace scram
--- src/bdd.h
+++ src/bdd.h
@@ -21,7 +21,11 @@
 #ifndef SCRAM_SRC_BDD_H_
 #define SCRAM_SRC_BDD_H_
 
+#include <array>
 #include <memory>
+#include <unordered_map>
+
+#include <boost/functional/hash.hpp>
 
 #include "boolean_graph.h"
 
@@ -48,14 +52,14 @@ class Terminal : public Vertex {
   bool value_;  ///< The meaning of the terminal.
 };
 
-/// @class NonTerminal
-/// Representation of non-terminal vertices in BDD graphs.
-class NonTerminal : public Vertex {
+/// @class Ite
+/// Representation of non-terminal if-then-else vertices in BDD graphs.
+class Ite : public Vertex {
  public:
   using VertexPtr = std::shared_ptr<Vertex>;
 
   /// @param[in] index Unique identifier of this non-terminal vertex.
-  explicit NonTerminal(int index);
+  explicit Ite(int index);
 
   /// @returns The index of this vertex.
   inline int index() const { return index_; }
@@ -82,6 +86,21 @@ class NonTerminal : public Vertex {
   VertexPtr low_;  ///< O (False/else) branch in the Shannon decomposition.
 };
 
+using IteTriplet = std::array<int, 3>;  ///< (v, G, H) triplet for functions.
+
+/// @struct IteHash
+/// Functor for hashing if-then-else nodes.
+struct IteHash : public std::unary_function<const IteTriplet, std::size_t> {
+  /// Operator overload for hashing if-then-else nodes.
+  ///
+  /// @param[in] triplet (v, G, H) nodes.
+  ///
+  /// @returns Hash value of the triplet.
+  std::size_t operator()(const IteTriplet& triplet) const noexcept {
+    return boost::hash_range(triplet.begin(), triplet.end());
+  }
+};
+
 /// @class Bdd
 /// Analysis of Boolean graphs with Binary Decision Diagrams.
 class Bdd {
@@ -95,7 +114,11 @@ class Bdd {
   void Analyze() noexcept;
 
  private:
+  using NodePtr = std::shared_ptr<Node>;
   using IGatePtr = std::shared_ptr<IGate>;
+  using VertexPtr = std::shared_ptr<Vertex>;
+  using TerminalPtr = std::shared_ptr<Terminal>;
+  using ItePtr = std::shared_ptr<Ite>;
 
   /// Assigns topological ordering to nodes of the Boolean Graph.
   /// The ordering is assigned to the optimization value of the nodes.
@@ -110,7 +133,40 @@ class Bdd {
   /// @note Optimization values must be clear before the assignment.
   int TopologicalOrder(const IGatePtr& root, int order) noexcept;
 
+  /// Converts all gates in the Boolean graph
+  /// into if-then-else BDD graphs.
+  /// Registers processed gates.
+  ///
+  /// @param[in] root The root or current parent gate of the graph.
+  ///
+  /// @warning Gate marks must be clear.
+  void ConvertGates(const IGatePtr& root) noexcept;
+
+  /// Turns a Boolean graph gate into a BDD graph.
+  /// Registers the results for future references.
+  ///
+  /// @param[in] gate The gate with a formula and arguments.
+  void ProcessGate(const IGatePtr& gate) noexcept;
+
+  /// Considers the Shannon decomposition
+  /// for the given Boolean operator
+  /// with ordered arguments.
+  ///
+  /// @param[in] type The operator or type of the gate.
+  /// @param[in] args Ordered arguments of the gate.
+  /// @param[in] pos The position of the argument to decompose against.
+  ///
+  /// @returns Pointer to the vertex in the BDD graph for the decomposition.
+  ItePtr IfThenElse(Operator type,
+                    const std::vector<std::pair<int, NodePtr>>& args,
+                    int pos) noexcept;
+
   BooleanGraph* fault_tree_;  ///< The main fault tree.
+  /// Table of unique if-then-else calculation results.
+  std::unordered_map<std::array<int, 3>, VertexPtr, IteHash> unique_table_;
+  std::unordered_map<int, ItePtr> gates_;  ///< Processed gates.
+  const TerminalPtr kOne_;  ///< Terminal True.
+  const TerminalPtr kZero_;  ///< Terminal False.
 };
 
 }  // namespace scram
