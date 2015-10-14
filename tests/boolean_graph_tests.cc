80c6b2eb99584e04f84cac994a47129a4ccafe9d
rakhimov <ol.rakhimov@gmail.com>
2015-10-13 18:05:24 -0700

    Add K/N IGate tests for duplicate arg addition

--- tests/boolean_graph_tests.cc
+++ tests/boolean_graph_tests.cc
@@ -53,6 +53,7 @@ class IGateAddArgTest : public ::testing::Test {
     var_two = std::make_shared<Variable>();
     var_three = std::make_shared<Variable>();
     vars_ = {var_one, var_two, var_three};
+    for (int i = 0; i < 2; ++i) vars_.emplace_back(new Variable());  // Extra.
   }
 
   /// Sets up the main gate with the default variables.
@@ -64,8 +65,8 @@ class IGateAddArgTest : public ::testing::Test {
   /// @note For K/N gates, K is set to 2 by default.
   void DefineGate(Operator type, int num_vars) {
     assert(type != kNotGate && type != kNullGate);
-    assert(num_vars < 4);
-    assert(!(type == kAtleastGate && num_vars < 3));
+    assert(num_vars < 6);
+    assert(!(type == kAtleastGate && num_vars < 2));
 
     g = std::make_shared<IGate>(type);
     if (type == kAtleastGate) g->vote_number(2);
@@ -154,6 +155,91 @@ TEST_F(IGateAddArgTest, DuplicateArgXor) {
   g->AddArg(var_one->index(), var_one);
   EXPECT_EQ(kNullState, g->state());
   EXPECT_TRUE(g->args().empty());
+}
+
+TEST_F(IGateAddArgTest, DuplicateArgAtleastToNull) {
+  DefineGate(kAtleastGate, 2);
+  g->AddArg(var_one->index(), var_one);
+  EXPECT_EQ(kNormalState, g->state());
+  EXPECT_EQ(kNullGate, g->type());
+  EXPECT_EQ(1, g->args().size());
+  EXPECT_EQ(var_two->index(), g->variable_args().begin()->first);
+}
+
+TEST_F(IGateAddArgTest, DuplicateArgAtleastToAnd) {
+  DefineGate(kAtleastGate, 3);
+  g->vote_number(3);  // K equals to the number of input arguments.
+  g->AddArg(var_one->index(), var_one);
+  EXPECT_EQ(kNormalState, g->state());
+  EXPECT_EQ(kAndGate, g->type());
+  EXPECT_EQ(2, g->args().size());
+  ASSERT_EQ(1, g->variable_args().size());
+  EXPECT_EQ(var_one->index(), g->variable_args().begin()->first);
+  ASSERT_EQ(1, g->gate_args().size());
+
+  IGatePtr sub = g->gate_args().begin()->second;
+  EXPECT_EQ(kOrGate, sub->type());  // Special case. K/N is in general.
+  EXPECT_EQ(1, sub->vote_number());  // This is the reason.
+  std::set<int> vars = {var_two->index(), var_three->index()};
+  EXPECT_EQ(vars, sub->args());
+  EXPECT_EQ(2, sub->variable_args().size());
+}
+
+TEST_F(IGateAddArgTest, DuplicateArgAtleastToOr_OneClone) {
+  DefineGate(kAtleastGate, 3);
+  g->vote_number(2);
+  g->AddArg(var_one->index(), var_one);
+  EXPECT_EQ(kNormalState, g->state());
+  EXPECT_EQ(kOrGate, g->type());
+  EXPECT_EQ(2, g->args().size());
+  ASSERT_EQ(1, g->variable_args().size());
+  EXPECT_EQ(var_one->index(), g->variable_args().begin()->first);
+  ASSERT_EQ(1, g->gate_args().size());
+
+  IGatePtr sub = g->gate_args().begin()->second;
+  EXPECT_EQ(kAndGate, sub->type());  // Special case. K/N is in general.
+  EXPECT_EQ(2, sub->vote_number());
+  EXPECT_EQ(2, sub->args().size());  // This is the reason.
+  std::set<int> vars = {var_two->index(), var_three->index()};
+  EXPECT_EQ(vars, sub->args());
+  EXPECT_EQ(2, sub->variable_args().size());
+}
+
+TEST_F(IGateAddArgTest, DuplicateArgAtleastToOr_TwoClones) {
+  DefineGate(kAtleastGate, 5);
+  g->vote_number(3);
+  g->AddArg(var_one->index(), var_one);
+  EXPECT_EQ(kNormalState, g->state());
+  EXPECT_EQ(kOrGate, g->type());
+  EXPECT_EQ(2, g->args().size());
+  EXPECT_TRUE(g->variable_args().empty());
+  ASSERT_EQ(2, g->gate_args().size());
+  auto it = g->gate_args().begin();
+  IGatePtr and_gate = it->second;  // Guessing.
+  ++it;
+  IGatePtr clone_one = it->second;  // Guessing.
+  // Correcting the guess.
+  if (and_gate->type() != kAndGate) std::swap(and_gate, clone_one);
+  ASSERT_EQ(kAndGate, and_gate->type());
+  ASSERT_EQ(kAtleastGate, clone_one->type());
+
+  EXPECT_EQ(kNormalState, clone_one->state());
+  EXPECT_EQ(3, clone_one->vote_number());
+  EXPECT_EQ(4, clone_one->args().size());
+  EXPECT_EQ(4, clone_one->variable_args().size());
+
+  EXPECT_EQ(kNormalState, and_gate->state());
+  EXPECT_EQ(2, and_gate->args().size());
+  ASSERT_EQ(1, and_gate->variable_args().size());
+  EXPECT_EQ(var_one->index(), and_gate->variable_args().begin()->first);
+  ASSERT_EQ(1, and_gate->gate_args().size());
+
+  IGatePtr clone_two = and_gate->gate_args().begin()->second;
+  EXPECT_EQ(kNormalState, clone_two->state());
+  EXPECT_EQ(kOrGate, clone_two->type());  // Special case. K/N is in general.
+  EXPECT_EQ(1, clone_two->vote_number());  // This is the reason.
+  EXPECT_EQ(4, clone_two->args().size());
+  EXPECT_EQ(4, clone_two->variable_args().size());
 }
 
 }  // namespace test
