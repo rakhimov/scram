9d137c2c35ba3f1bba92333c1289b6ad366de9fe
rakhimov <ol.rakhimov@gmail.com>
2016-02-11 19:05:45 -0800

    Fix improper 'Prob' and 'FTA' in test names

--- tests/initializer_tests.cc
+++ tests/initializer_tests.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2015 Olzhas Rakhimov
+ * Copyright (C) 2014-2016 Olzhas Rakhimov
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -83,7 +83,7 @@ TEST(InitializerTest, UnsupportedFeature) {
 }
 
 // Test correct inputs without probability information.
-TEST(InitializerTest, CorrectFTAInputs) {
+TEST(InitializerTest, CorrectFtaInputs) {
   std::string dir = "./share/scram/input/fta/";
   std::vector<std::string> correct_inputs = {
       "correct_tree_input.xml",
@@ -113,7 +113,7 @@ TEST(InitializerTest, CorrectFTAInputs) {
 }
 
 // Test correct inputs with probability information.
-TEST(InitializerTest, CorrectProbInputs) {
+TEST(InitializerTest, CorrectProbabilityInputs) {
   std::string dir = "./share/scram/input/fta/";
   std::vector<std::string> correct_inputs = {
       "correct_tree_input_with_probs.xml",
@@ -133,7 +133,7 @@ TEST(InitializerTest, CorrectProbInputs) {
 }
 
 // Test incorrect fault tree inputs
-TEST(InitializerTest, IncorrectFTAInputs) {
+TEST(InitializerTest, IncorrectFtaInputs) {
   std::string dir = "./share/scram/input/fta/";
 
   std::vector<std::string> incorrect_inputs = {
@@ -185,7 +185,7 @@ TEST(InitializerTest, IncorrectFTAInputs) {
   }
 }
 
-TEST(InitializerTest, IncorrectProbInputs) {
+TEST(InitializerTest, IncorrectProbabilityInputs) {
   std::string dir = "./share/scram/input/fta/";
   std::vector<std::string> incorrect_inputs = {
       "invalid_probability.xml",
