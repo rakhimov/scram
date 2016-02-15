09d5cca5d6a7cbe841f46a5bf853de3889900af6
rakhimov <ol.rakhimov@gmail.com>
2016-02-15 09:52:58 -0800

    Refactor Travis-CI shell scripts

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,11 +14,10 @@ sudo apt-get install -qq python-lxml
 sudo apt-get install -qq graphviz
 sudo pip install nose
 
-if [[ -z "${RELEASE}" && "$CXX" = "g++" ]]; then
-  sudo apt-get install -qq valgrind
-  sudo apt-get install -qq doxygen
-  sudo pip install cpp-coveralls
-  sudo pip install lizard
-  sudo pip install codecov
-  sudo pip install coverage
-fi
+[[ -z "${RELEASE}" && "$CXX" = "g++" ]] || exit 0
+sudo apt-get install -qq valgrind
+sudo apt-get install -qq doxygen
+sudo pip install cpp-coveralls
+sudo pip install lizard
+sudo pip install codecov
+sudo pip install coverage
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -ev
+
 if [[ -n "${RELEASE}" ]]; then
   ./install.py -r --prefix=./install --threads 2
 elif [[ "$CXX" = "g++" ]]; then
@@ -11,42 +12,42 @@ fi
 
 ./.travis/run_tests.sh
 
-if [[ -z "${RELEASE}" && "$CXX" = "g++" ]]; then
-  # Check for memory leaks with Valgrind
-  valgrind --tool=memcheck --leak-check=full --show-leak-kinds=definite \
-    --errors-for-leak-kinds=definite --error-exitcode=127 \
-    --track-fds=yes \
-    scram_tests \
-    --gtest_filter=-*Death*:*Baobab*:*IncorrectFTAInputs:GateTest.Cycle \
-    || [[ $? -ne 127 ]]
+[[ -z "${RELEASE}" && "$CXX" = "g++" ]] || exit 0
+# Check for memory leaks with Valgrind
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=definite \
+  --errors-for-leak-kinds=definite --error-exitcode=127 \
+  --track-fds=yes \
+  scram_tests \
+  --gtest_filter=-*Death*:*Baobab*:*IncorrectFtaInputs:GateTest.Cycle \
+  || [[ $? -ne 127 ]]
 
-  # Submit coverage of C++
-  coveralls --exclude tests  --exclude ./build/CMakeFiles/ \
-    --exclude ./build/lib/ --exclude ./build/gui/ \
-    --exclude ./build/bin/  --exclude install/include/ \
-    --exclude gui/ \
-    --exclude src/error.cc \
-    --exclude src/env.cc \
-    --exclude src/version.cc \
-    --exclude src/logger.cc \
-    --exclude-pattern .*/src/.*\.h$ \
-    > /dev/null
+# Submit coverage of C++
+coveralls --exclude tests  --exclude ./build/CMakeFiles/ \
+  --exclude ./build/lib/ --exclude ./build/gui/ \
+  --exclude ./build/bin/  --exclude install/include/ \
+  --exclude gui/ \
+  --exclude src/error.cc \
+  --exclude src/env.cc \
+  --exclude src/version.cc \
+  --exclude src/logger.cc \
+  --exclude-pattern .*/src/.*\.h$ \
+  > /dev/null
 
-  # Submit coverage of Python
-  codecov > /dev/null
+# Submit coverage of Python
+codecov > /dev/null
 
-  # Check documentation coverage
-  doxygen ./.travis/doxygen.conf > /dev/null 2> doc_errors.txt
-  # Deletion of compiler generated default functions
-  sed -i '/=delete/d' doc_errors.txt
-  # Doxygen 1.8.6 can't deal with C++11 initializer list in constructor.
-  sed -i '/expression\.cc/d' doc_errors.txt
-  if [[ -s doc_errors.txt ]]; then
-    echo "Documentation errors:" >&2
-    cat doc_errors.txt >&2
-    exit 1
-  fi
-
-  # Lizard function complexity printout
-  ( cd ./src && ../scripts/lizard_report.sh | tail -n 30 )
+# Check documentation coverage
+doxygen ./.travis/doxygen.conf > /dev/null 2> doc_errors.txt
+# Deletion of compiler generated default functions
+sed -i '/=delete/d' doc_errors.txt
+# Doxygen 1.8.6 can't deal with C++11 initializer list in constructor.
+sed -i '/expression\.cc/d' doc_errors.txt
+if [[ -s doc_errors.txt ]]; then
+  echo "Documentation errors:" >&2
+  cat doc_errors.txt >&2
+  exit 1
 fi
+
+# Lizard function complexity printout for C++ and Python
+( cd ./src && ../scripts/lizard_report.sh | tail -n 30 )
+lizard -s cyclomatic_complexity -L 60 -a 5 scripts/*.py | tail -n 20
