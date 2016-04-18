162f346314b9b7ce8f7fadd1997a6e9621cdf401
rakhimov <ol.rakhimov@gmail.com>
2016-04-17 18:28:32 -0700

    Configure Travis-CI on Trusty with add-ons
    It is more convenient than
    manually calling 'sudo apt-get install -y package'.

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,16 +2,6 @@
 
 set -ev
 
-sudo apt-get install -qq libboost-program-options-dev
-sudo apt-get install -qq libboost-filesystem-dev
-sudo apt-get install -qq libboost-system-dev
-sudo apt-get install -qq libboost-math-dev
-sudo apt-get install -qq libxml2-dev
-sudo apt-get install -qq libxml++2.6-dev
-sudo apt-get install -qq libgoogle-perftools-dev
-sudo apt-get install -qq qt5-default
-sudo apt-get install -qq python-lxml
-sudo apt-get install -qq graphviz
 sudo pip install nose
 
 [[ -z "${RELEASE}" && "$CXX" = "g++" ]] || exit 0
