f538a57c2b22c20f8e6b037b7ae60d70fdd20258
rakhimov <ol.rakhimov@gmail.com>
2016-04-21 11:43:38 -0700

    Filter Doxygen warnings confused with C++ macros

--- .travis/script.sh
+++ .travis/script.sh
@@ -42,6 +42,7 @@ doxygen ./.travis/doxygen.conf > /dev/null 2> doc_errors.txt
 sed -i '/=delete/d' doc_errors.txt
 # Doxygen 1.8.6 can't deal with C++11 initializer list in constructor.
 sed -i '/expression\.cc/d' doc_errors.txt
+sed -i '/env\.cc.*: warning:/d' doc_errors.txt
 if [[ -s doc_errors.txt ]]; then
   echo "Documentation errors:" >&2
   cat doc_errors.txt >&2
