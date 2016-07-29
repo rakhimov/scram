1ab084ceca6aa81344c641c63404298afb2accde
rakhimov <ol.rakhimov@gmail.com>
2016-07-28 19:31:54 -0700

    GUI: Setup Qt translations support in CMake
    The code is taken from Subsurface:
    https://github.com/torvalds/subsurface/
    
    The translations are only build
    if Qt5 Linguist is found on the platform.
    Ubuntu 14.04 doesn't have CMake configurations for Linguist.

---
+++ gui/translations/scram_untranslated.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>scram::gui::MainWindow</name>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About the Qt toolkit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
