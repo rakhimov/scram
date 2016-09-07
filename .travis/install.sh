02af0ebbe7b1e0751ab49b0bb2a78e6e3eeba753
rakhimov <ol.rakhimov@gmail.com>
2016-09-07 06:43:04 -0700

    Download Boost 1.56 from scram deps on Sourceforge
    Pre-built packages, instead of building on Travis.

--- .travis/install.sh
+++ .travis/install.sh
@@ -23,17 +23,11 @@ fi
 
 # Installing dependencies from source.
 PROJECT_DIR=$PWD
-mkdir deps
-cd deps
-mkdir install
-INSTALL_PATH=$PWD/install
-export PATH=$INSTALL_PATH/include:$INSTALL_PATH/lib:$PATH
-
-wget https://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2
-tar --bzip2 -xf boost_1_56_0.tar.bz2
-cd boost_1_56_0
-./bootstrap.sh --prefix=$INSTALL_PATH --with-libraries=math,filesystem,program_options,system,date_time
-./b2 link=shared threading=single variant=release -j2 -q install
+cd /tmp
+wget https://sourceforge.net/projects/iscram/files/deps/boost-scram.tar.bzip2
+tar -xf ./boost-scram.tar.bzip2
+sudo mv ./install/lib/* /usr/lib/
+sudo mv ./install/include/boost /usr/include
 cd $PROJECT_DIR
 
 [[ -z "${RELEASE}" && "$CXX" = "g++" ]] || exit 0
