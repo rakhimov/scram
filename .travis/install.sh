b9284127508c72fa059da490ae54775dad97ff8c
rakhimov <ol.rakhimov@gmail.com>
2016-09-07 03:26:57 -0700

    Install boost from source on Travis-CI
    [skip appveyor]

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,6 +21,21 @@ if [[ "$CXX" == "icpc" ]]; then
   ./icc-travis/install-icc.sh
 fi
 
+# Installing dependencies from source.
+PROJECT_DIR=$PWD
+mkdir deps
+cd deps
+mkdir install
+INSTALL_PATH=$PWD/install
+export PATH=$INSTALL_PATH/include:$INSTALL_PATH/lib:$PATH
+
+wget https://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2
+tar --bzip2 -xf boost_1_56_0.tar.bz2
+cd boost_1_56_0
+./bootstrap.sh --prefix=$INSTALL_PATH --with-libraries=math,filesystem,program_options,system,date_time
+./b2 link=shared threading=single variant=release -j2 -q install
+cd $PROJECT_DIR
+
 [[ -z "${RELEASE}" && "$CXX" = "g++" ]] || exit 0
 sudo apt-get install -qq ggcov
 sudo apt-get install -qq valgrind
