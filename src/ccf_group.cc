803abe52d391eb2af82458c997a74de57d8b5600
rakhimov <ol.rakhimov@gmail.com>
2016-12-20 05:20:36 -0800

    Implement std::prev_permutation based N-choose-K
    The permutations of the bitmask is used to generate combinations.
    This approach still preserves the order guarantees
    of the stack-based combination generator.
    
    The advantage of this generator is that it is an iterator.
    It can be used in a simple for range loop.

--- src/ccf_group.cc
+++ src/ccf_group.cc
@@ -20,6 +20,8 @@
 
 #include "ccf_group.h"
 
+#include <algorithm>
+
 #include <boost/range/algorithm.hpp>
 
 #include "expression/arithmetic.h"
@@ -91,62 +93,80 @@ void CcfGroup::Validate() const {
 
 namespace {
 
-/// Helper function to generate combinations of elements from a range.
+/// Generator of K out-of N combinations.
 ///
-/// @tparam Iterator  Iterator type of the range.
-///                   Best if it's a random access iterator.
-/// @tparam Functor  The function type with
-///                  operator()(const std::vector<value_type>&)
-///
-/// @param[in] k  The number of elements to choose into a combination.
-/// @param[in] first1  The beginning of the range.
-/// @param[in] last1  The end of the range.
-/// @param[in] combination  The current combination stack of elements.
-/// @param[in] combination_handler  The function working on a combination.
-template <typename Iterator, typename Functor>
-void GenerateCombinations(
-    int k, Iterator first1, Iterator last1,
-    std::vector<typename Iterator::value_type>* combination,
-    Functor& combination_handler) {  // NOLINT(runtime/references)
-  using container_type = std::vector<typename Iterator::value_type>;
-  assert(k >= 0 && "No negative choice number.");
-  auto n = std::distance(first1, last1);
-  if (k > n)
-    return;
-  if (k == 0) {
-    combination_handler(static_cast<const container_type&>(*combination));
-  } else if (k == n) {
-    combination->insert(combination->end(), first1, last1);
-    combination_handler(static_cast<const container_type&>(*combination));
-    combination->erase(std::prev(combination->end(), n), combination->end());
-  } else if (first1 != last1) {
-    combination->push_back(*first1);
-    GenerateCombinations(k - 1, std::next(first1), last1, combination,
-                         combination_handler);
-    combination->pop_back();
-    GenerateCombinations(k, std::next(first1), last1, combination,
-                         combination_handler);
-  }
-}
+/// The combination generator guarantees the element order
+/// to be the same as in the original source collection.
+template <typename Iterator>
+class CombinationGenerator {
+ public:
+  /// Read forward-iterator for combination generation.
+  class iterator {
+    friend bool operator==(const iterator& lhs, const iterator& rhs) {
+      return lhs.first1_ == rhs.first1_ && lhs.bitmask_ == rhs.bitmask_;
+    }
+    friend bool operator!=(const iterator& lhs, const iterator& rhs) {
+      return !(lhs == rhs);
+    }
 
-/// Generates combinations of elements from a range.
-///
-/// @tparam Iterator  Iterator type of the range.
-///                   Best if it's a random access iterator.
-/// @tparam Functor  The function type with
-///                  operator()(const std::vector<value_type>&)
-///
-/// @param[in] k  The number of elements to choose into a combination.
-/// @param[in] first1  The beginning of the range.
-/// @param[in] last1  The end of the range.
-/// @param[in] combination_handler  The function working on a combination.
-template <typename Iterator, typename Functor>
-void GenerateCombinations(int k, Iterator first1, Iterator last1,
-                          Functor& combination_handler) {  // NOLINT
-  assert(k >= 0 && "No negative choice number.");
-  std::vector<typename Iterator::value_type> combination;
-  combination.reserve(std::distance(first1, last1));
-  GenerateCombinations(k, first1, last1, &combination, combination_handler);
+   public:
+    /// @copydoc CombinationGenerator::CombinationGenerator
+    iterator(int k, Iterator first1, Iterator last1)
+        : first1_(first1), bitmask_(std::distance(first1, last1)) {
+      assert(k > 0 && "The choice must be positive.");
+      assert(k <= std::distance(first1, last1) && "The choice can't exceed N.");
+      std::fill_n(bitmask_.begin(), k, 1);
+    }
+
+    /// Constructs a special iterator to signal the end of generation.
+    explicit iterator(Iterator first1) : first1_(first1) {}
+
+    /// Sets up the iterator state for the next combination.
+    iterator& operator++() {
+      if (boost::prev_permutation(bitmask_) == false)
+        bitmask_.clear();
+      return *this;
+    }
+
+    /// @returns The current combination of k elements.
+    std::vector<typename Iterator::value_type> operator*() {
+      assert(!bitmask_.empty() && "Calling on the sentinel iterator.");
+      std::vector<typename Iterator::value_type> combination;
+      for (int i = 0; i < bitmask_.size(); ++i) {
+        if (bitmask_[i])
+          combination.push_back(*std::next(first1_, i));
+      }
+      return combination;
+    }
+
+   private:
+    Iterator first1_;  ///< The first element in the collection.
+    std::vector<int> bitmask_;  ///< bit-mask for N elements.
+  };
+
+  /// Constructor for a range with N elements to choose from.
+  ///
+  /// @param[in] k  The number of elements to choose.
+  /// @param[in] first1  The start of the range.
+  /// @param[in] last1  The sentinel end of the range.
+  CombinationGenerator(int k, Iterator first1, Iterator last1)
+      : k_(k), first1_(first1), last1_(last1) {}
+
+  /// @returns The generator iterator from the first combination.
+  iterator begin() const { return iterator(k_, first1_, last1_); }
+  /// @returns The sentinel end signal iterator.
+  iterator end() const { return iterator(first1_); }
+
+ private:
+  int k_;  ///< The combination size.
+  Iterator first1_;  ///< The begin of the range.
+  Iterator last1_;  ///< The end of the range.
+};
+
+/// Helper for CombinationGenerator construction to deduce iterator type.
+template <typename Iterator>
+auto make_combination_generator(int k, Iterator first1, Iterator last1) {
+  return CombinationGenerator<Iterator>(k, first1, last1);
 }
 
 /// Joins CCF combination proxy gate names
@@ -197,15 +217,15 @@ void CcfGroup::ApplyModel() {
   for (auto& entry : probabilities) {
     int level = entry.first;
     ExpressionPtr prob = entry.second;
-    auto combination_handler = [this, &prob](std::vector<Gate*> combination) {
+    for (auto combination : make_combination_generator(level,
+                                                       proxy_gates.begin(),
+                                                       proxy_gates.end())) {
       auto ccf_event = std::make_shared<CcfEvent>(JoinNames(combination), this);
       ccf_event->expression(prob);
       for (Gate* gate : combination)
         gate->formula().AddArgument(ccf_event);
       ccf_event->members(std::move(combination));  // Move, at last.
-    };
-    GenerateCombinations(level, proxy_gates.begin(), proxy_gates.end(),
-                         combination_handler);
+    }
   }
 }
 
