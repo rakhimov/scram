37faa26d4e41b3910c85d9924a47b4e8eec13905
rakhimov <ol.rakhimov@gmail.com>
2017-06-29 21:53:53 -0700

    GUI: Add stack widget for Event Dialog type data
    The House and Basic event expressions can be set
    using the type data widgets.
    The actual data retrieval and setting are not implemented.

--- gui/eventdialog.cpp
+++ gui/eventdialog.cpp
@@ -17,10 +17,49 @@
 
 #include "eventdialog.h"
 
+#include <limits>
+
+#include <QDoubleValidator>
+#include <QObject>
+
+#include "guiassert.h"
+
 namespace scram {
 namespace gui {
 
-EventDialog::EventDialog(QWidget *parent) : QDialog(parent) { setupUi(this); }
+#define OVERLOAD(type, name, ...)                                              \
+    static_cast<void (type::*)(__VA_ARGS__)>(&type::name)
+
+EventDialog::EventDialog(QWidget *parent) : QDialog(parent) {
+    setupUi(this);
+
+    constantValue->setValidator(new QDoubleValidator(0, 1, 100, this));
+    exponentialRate->setValidator(
+        new QDoubleValidator(0, std::numeric_limits<double>::max(), 100, this));
+
+    connect(typeBox, OVERLOAD(QComboBox, currentIndexChanged, int),
+            [this](int index) {
+                switch (index) {
+                case 0:
+                    GUI_ASSERT(typeBox->currentText() == tr("House event"), );
+                    stackedWidgetType->setCurrentWidget(tabBoolean);
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    stackedWidgetType->setCurrentWidget(tabExpression);
+                    break;
+                default:
+                    GUI_ASSERT(false, );
+                }
+            });
+
+    // Ensure proper defaults.
+    GUI_ASSERT(typeBox->currentIndex() == 0, );
+    GUI_ASSERT(stackedWidgetType->currentIndex() == 0, );
+    GUI_ASSERT(expressionType->currentIndex() == 0, );
+    GUI_ASSERT(stackedWidgetExpressionData->currentIndex() == 0, );
+}
 
 } // namespace gui
 } // namespace scram
