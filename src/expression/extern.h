b944bd0f421ee9ee395a9e82896fc731284f4220
rakhimov <ol.rakhimov@gmail.com>
2017-08-17 22:39:53 -0700

    Add mef::ExternLibrary class to support dll load
    This class provides a wrapper
    to handle loading dynamic libraries at runtime
    and querying symbols from libraries.
    
    The cross-platform code relies on Boost DLL library
    available since 1.61.
    Unfortunately, since Ubuntu 16.04 has only 1.58 Boost version,
    dynamic loading is implemented using native Linux/POSIX API.
    
    Issue #74

---
+++ src/expression/extern.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2017 Olzhas Rakhimov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// @file extern.h
+/// The MEF facilities to call external functions in expressions.
+
+#ifndef SCRAM_SRC_EXPRESSION_EXTERN_H_
+#define SCRAM_SRC_EXPRESSION_EXTERN_H_
+
+#include <string>
+#include <type_traits>
+
+#include <boost/filesystem/path.hpp>
+#include <boost/noncopyable.hpp>
+
+#include "src/element.h"
+#include "src/expression.h"
+
+namespace scram {
+namespace mef {
+
+/// The MEF construct to extend expressions with external libraries.
+/// This class dynamically loads and manages libraries.
+/// It supports only very basic interface for C function lookup with its symbol.
+class ExternLibrary : public Element, private boost::noncopyable {
+ public:
+  /// @copydoc Element::Element
+  ///
+  /// @param[in] lib_path  The library path with its name.
+  /// @param[in] reference_dir  The reference directory for relative paths.
+  /// @param[in] system  Search for the library in system paths.
+  /// @param[in] decorate  Decorate the library name with prefix and suffix.
+  ///
+  /// @throws IOError  The library cannot be found.
+  ///
+  /// @pre The library path is canonical file path.
+  ExternLibrary(std::string name, std::string lib_path,
+                const boost::filesystem::path& reference_dir, bool system,
+                bool decorate);
+
+  ~ExternLibrary();  ///< Closes the loaded library.
+
+  /// @tparam F  The C free function pointer type.
+  ///
+  /// @param[in] symbol  The function symbol in the library.
+  ///
+  /// @returns The function pointer resolved from the symbol.
+  ///
+  /// @throws UndefinedElement  The symbol is not in the library.
+  template <typename F>
+  std::enable_if_t<std::is_pointer<F>::value &&
+                       std::is_function<std::remove_pointer_t<F>>::value,
+                   F>
+  get(const std::string& symbol) const {
+    return static_cast<F>(get(symbol.c_str()));
+  }
+
+ private:
+  /// Hides all the cross-platform shared library faculties.
+  /// @todo Remove/refactor after switching to Boost 1.61 on Linux.
+  class Pimpl;
+
+  /// @param[in] symbol  The function symbol in the library.
+  ///
+  /// @returns The function loaded from the library symbol.
+  ///
+  /// @throws UndefinedElement  The symbol is not in the library.
+  void* get(const char* symbol) const;
+
+  Pimpl* pimpl_;  ///< Provides basic implementation for function discovery.
+};
+
+}  // namespace mef
+}  // namespace scram
+
+#endif  // SCRAM_SRC_EXPRESSION_EXTERN_H_
