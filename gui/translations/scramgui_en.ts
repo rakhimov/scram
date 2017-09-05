1d77330725658c05710109385a54cc512e340b58
rakhimov <ol.rakhimov@gmail.com>
2017-09-05 02:15:51 -0700

    GUI: Update the translation source file

--- gui/translations/scramgui_en.ts
+++ gui/translations/scramgui_en.ts
@@ -21,30 +21,31 @@
     </message>
     <message>
         <source>House event</source>
+        <extracomment>Constant event w/ Boolean state value</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Basic event</source>
+        <extracomment>The main analysis event (e.g., component failure)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Undeveloped</source>
+        <extracomment>The basic event flavor that indicates the further developement into a gate is possible.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Conditional</source>
+        <extracomment>Conditioning flavor for a basic event (used as a conditioning variable for conditional probabilities)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Gate</source>
+        <extracomment>The intermediary w/ a Boolean formula as a definition. Defines the logic and interdependencies of the fault tree.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>State:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>State combo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -57,14 +58,6 @@
     </message>
     <message>
         <source>Expression</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Constant</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Exponential</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -89,10 +82,12 @@
     </message>
     <message>
         <source>ATLEAST</source>
+        <extracomment>VOTE, Voting OR, Combo, Combination, K/N gate</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>XOR</source>
+        <extracomment>Exclusive OR</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -101,14 +96,12 @@
     </message>
     <message>
         <source>NAND</source>
+        <extracomment>NOT AND</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>NOR</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>NULL</source>
+        <extracomment>NOT OR</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -139,19 +132,20 @@
         <source>Model</source>
         <translation type="unfinished"></translation>
     </message>
-</context>
-<context>
-    <name>GuardedApplication</name>
     <message>
-        <source>Internal SCRAM Error</source>
+        <source>Constant</source>
+        <comment>constant probability value</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Internal Exception Error</source>
+        <source>Exponential</source>
+        <comment>probability type defined w/ negative exponential formula</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Unknown exception type.</source>
+        <source>NULL</source>
+        <comment>Not a null value.</comment>
+        <extracomment>Pass-through, NO-OP type</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -274,43 +268,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>400%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>200%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>150%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>125%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>100%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>85%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>50%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>70%</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Best &amp;Fit</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&amp;Run</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -319,10 +277,6 @@
     </message>
     <message>
         <source>Alt+R</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&amp;Settings...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -365,6 +319,16 @@
         <source>Re&amp;name Model</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>&amp;Run</source>
+        <comment>execute analysis</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Settings...</source>
+        <comment>analysis configuration</comment>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>NameDialog</name>
@@ -379,40 +343,6 @@
 </context>
 <context>
     <name>QObject</name>
-    <message>
-        <source>Unrecoverable Internal Error</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>SIGSEGV: Invalid memory access.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>SIGFPE: Erroneous arithmetic operation.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>SIGILL: Illegal instruction.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>SCRAM exception: %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Standard exception: %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Exception of unknown type: no message available.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Exception no-throw contract violation:
-
-%1</source>
-        <translation type="unfinished"></translation>
-    </message>
     <message>
         <source>Set element &apos;%1&apos; label to &apos;%2&apos;</source>
         <translation type="unfinished"></translation>
@@ -481,10 +411,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>MCUB</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Analyses</source>
         <translation type="unfinished"></translation>
     </message>
@@ -502,18 +428,6 @@
     </message>
     <message>
         <source>Algorithms</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>MOCUS</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>BDD</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>ZBDD</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -567,18 +481,22 @@
     </message>
     <message>
         <source>The argument &apos;%1&apos; is already in formula.</source>
+        <extracomment>Duplicate arguments are not allowed in a formula.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>The argument &apos;%1&apos; would introduce a self-cycle.</source>
+        <extracomment>Self-cycle is also called a loop in a graph.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>The argument &apos;%1&apos; would introduce a cycle.</source>
+        <extracomment>Fault trees are acyclic graphs.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>The event with name &apos;%1&apos; already exists.</source>
+        <extracomment>Duplicate event definition in the model.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -598,12 +516,16 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>%1 connective requires at-least %2 arguments.</source>
+        <source>Fault tree &apos;%1&apos; is already defined with a top gate.</source>
+        <extracomment>Fault tree redefinition.</extracomment>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>Fault tree &apos;%1&apos; is already defined with a top gate.</source>
-        <translation type="unfinished"></translation>
+    <message numerus="yes">
+        <source>%1 connective requires at-least %n arguments.</source>
+        <extracomment>The number of required arguments is always more than 2.</extracomment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
     </message>
 </context>
 <context>
@@ -618,6 +540,7 @@
     </message>
     <message>
         <source>Running analysis...</source>
+        <extracomment>This is a message shown during the analysis run.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -626,22 +549,21 @@
     </message>
     <message>
         <source>Initialization Error</source>
+        <extracomment>The error upon initialization from a file.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Fault tree &apos;%1&apos; must have a single top-gate.</source>
+        <extracomment>Single top/root event fault tree are expected by GUI.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Find/Filter (Perl Regex)</source>
+        <extracomment>The search bar.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>About SCRAM</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&lt;h1&gt;SCRAM %1&lt;/h1&gt;The GUI front-end for SCRAM,&lt;br/&gt;a command-line risk analysis multi-tool.&lt;br/&gt;&lt;br/&gt;License: GPLv3+&lt;br/&gt;Homepage: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br/&gt;Technical Support: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;&lt;br/&gt;Bug Tracker: &lt;a href=&quot;%4&quot;&gt;%4&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -677,10 +599,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Save Error</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Export Report As</source>
         <translation type="unfinished"></translation>
     </message>
@@ -690,6 +608,7 @@
     </message>
     <message>
         <source>Dependency Container Removal</source>
+        <extracomment>The container w/ dependents still in the model.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -702,6 +621,7 @@
     </message>
     <message>
         <source>Dependency Event Removal</source>
+        <extracomment>The event w/ dependents in the model.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -710,26 +630,32 @@
     </message>
     <message>
         <source>Add fault tree &apos;%1&apos; with gate &apos;%2&apos;</source>
+        <extracomment>Addition of a fault by defining its root event first.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Gates</source>
+        <extracomment>The tab for the table of gates.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Basic Events</source>
+        <extracomment>The tab for the table of basic events.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>House Events</source>
+        <extracomment>The tab for the table of house events.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Fault Tree: %1</source>
+        <extracomment>The tab for a fault tree diagram.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Products: %L1</source>
+        <extracomment>Cut-sets or prime-implicants (depending on the settings).</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -758,6 +684,7 @@
     </message>
     <message>
         <source>Importance Factors: %L1</source>
+        <extracomment>The number of important events w/ factors defined.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -792,23 +719,36 @@
         <source>Importance: %1</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>&lt;h1&gt;SCRAM %1&lt;/h1&gt;The GUI front-end for SCRAM,&lt;br/&gt;a command-line risk analysis multi-tool.&lt;br/&gt;&lt;br/&gt;License: GPLv3+&lt;br/&gt;Homepage: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br/&gt;Technical Support: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;&lt;br/&gt;Bug Tracker: &lt;a href=&quot;%4&quot;&gt;%4&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;%5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save Error</source>
+        <comment>error on saving to file</comment>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>scram::gui::ModelTree</name>
     <message>
         <source>Fault Trees</source>
+        <extracomment>The parent item for collections of fault trees in the model.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Gates</source>
+        <extracomment>The table of gates.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Basic Events</source>
+        <extracomment>The table of basic events.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>House Events</source>
+        <extracomment>The table of house events.</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -828,6 +768,7 @@
     </message>
     <message>
         <source>Conditional</source>
+        <extracomment>Actually, this is &apos;conditioning&apos; since the event is the condition for some sub-tree.</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -839,10 +780,12 @@
     </message>
     <message>
         <source>Flavor</source>
+        <extracomment>The flavor type of a basic event.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Probability</source>
+        <extracomment>In PRA context, probability may be unavailability or unreliability.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -862,6 +805,7 @@
     </message>
     <message>
         <source>at-least %1</source>
+        <extracomment>Also named as &apos;vote&apos;, &apos;voting or&apos;, &apos;combination&apos;, &apos;combo&apos;.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -874,14 +818,17 @@
     </message>
     <message>
         <source>null</source>
+        <extracomment>This is &apos;pass-through&apos; or &apos;no-action&apos; gate type.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>nand</source>
+        <extracomment>not and.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>nor</source>
+        <extracomment>not or.</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -893,10 +840,12 @@
     </message>
     <message>
         <source>Connective</source>
+        <extracomment>Boolean operator of the Boolean formula.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Args</source>
+        <extracomment>The number of arguments in the Boolean formula.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -912,6 +861,7 @@
     </message>
     <message>
         <source>State</source>
+        <extracomment>House event Boolean state.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
