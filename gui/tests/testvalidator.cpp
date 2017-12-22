02a084b08b4604438449dab990bb90dab28aec6c
rakhimov <ol.rakhimov@gmail.com>
2017-12-22 03:30:30 -0800

    GUI: Wrap QTestData initialization w/ C++11 helper
    Initializer list, tuples, variadic templates help
    remove repetitive code of QTest::addColumn and QTest::newRow.
    
    Issue #202

--- gui/tests/testvalidator.cpp
+++ gui/tests/testvalidator.cpp
@@ -19,6 +19,8 @@
 
 #include <QtTest>
 
+#include "data.h"
+
 Q_DECLARE_METATYPE(QValidator::State)
 
 namespace QTest {
@@ -49,117 +51,107 @@ class TestValidator : public QObject
 private slots:
     void testName_data()
     {
-        QTest::addColumn<QString>("value");
-        QTest::addColumn<QValidator::State>("state");
-
-#if QT_VERSION >= 0x050500 ///< @todo Upgrade Qt.
-        QTest::newRow("empty") << "" << QValidator::Intermediate;
-#endif
-        QTest::newRow("letter") << "a" << QValidator::Acceptable;
-        QTest::newRow("number") << "1" << QValidator::Invalid;
-        QTest::newRow("hyphen") << "-" << QValidator::Invalid;
-        QTest::newRow("underscore") << "_" << QValidator::Invalid;
-        QTest::newRow("space") << " " << QValidator::Invalid;
-        QTest::newRow("dot") << "." << QValidator::Invalid;
-        QTest::newRow("w/ space") << "pump one" << QValidator::Invalid;
-        QTest::newRow("w/ tab") << "pump\tone" << QValidator::Invalid;
-        QTest::newRow("w/ dot") << "pump.one" << QValidator::Invalid;
-        QTest::newRow("w/ colon") << "pump:one" << QValidator::Invalid;
-        QTest::newRow("w/ hyphen") << "pump-one" << QValidator::Acceptable;
-        QTest::newRow("w/ leading hyphen") << "-pump" << QValidator::Invalid;
-        QTest::newRow("trailing hyphen") << "pump-" << QValidator::Intermediate;
-        QTest::newRow("w/ under") << "pump_one" << QValidator::Acceptable;
-        QTest::newRow("w/ leading under") << "_pump" << QValidator::Invalid;
-        QTest::newRow("w/ trailing under") << "pump_" << QValidator::Acceptable;
-        QTest::newRow("w/ number") << "pump1" << QValidator::Acceptable;
-        QTest::newRow("w/ leading number") << "1pump" << QValidator::Invalid;
-        QTest::newRow("w/ leading !") << "!pump1" << QValidator::Invalid;
-        QTest::newRow("w/ leading ~") << "~pump1" << QValidator::Invalid;
-        QTest::newRow("w/ trailing ~") << "pump1~" << QValidator::Invalid;
-        QTest::newRow("w/ ~ separator") << "pump~one" << QValidator::Invalid;
-        QTest::newRow("w/ double hyphen") << "pump--one" << QValidator::Invalid;
-        QTest::newRow("w/ dunder") << "pump__one" << QValidator::Acceptable;
-        QTest::newRow("hyphen/under") << "pump-_one" << QValidator::Acceptable;
-        QTest::newRow("under/hyphen") << "pump_-one" << QValidator::Acceptable;
-        QTest::newRow("dunder/dunder") << "__pump__" << QValidator::Invalid;
-        QTest::newRow("end dunder") << "pump__" << QValidator::Acceptable;
-        QTest::newRow("w/ hyphen num") << "pump-1" << QValidator::Acceptable;
-        QTest::newRow("w/ under num") << "pump_1" << QValidator::Acceptable;
-        QTest::newRow("capital") << "PUMP" << QValidator::Acceptable;
-        QTest::newRow("lower") << "pump" << QValidator::Acceptable;
-        QTest::newRow("mixed") << "PumpOne" << QValidator::Acceptable;
-        QTest::newRow("camel") << "pumpOne" << QValidator::Acceptable;
-        QTest::newRow("non-English") << u8"Помпа" << QValidator::Acceptable;
+        populateData<QString, QValidator::State>(
+            {"value", "state"},
+            {{"letter", "a", QValidator::Acceptable},
+             {"number", "1", QValidator::Invalid},
+             {"hyphen", "-", QValidator::Invalid},
+             {"underscore", "_", QValidator::Invalid},
+             {"space", " ", QValidator::Invalid},
+             {"dot", ".", QValidator::Invalid},
+             {"w/ space", "pump one", QValidator::Invalid},
+             {"w/ tab", "pump\tone", QValidator::Invalid},
+             {"w/ dot", "pump.one", QValidator::Invalid},
+             {"w/ colon", "pump:one", QValidator::Invalid},
+             {"w/ hyphen", "pump-one", QValidator::Acceptable},
+             {"w/ leading hyphen", "-pump", QValidator::Invalid},
+             {"trailing hyphen", "pump-", QValidator::Intermediate},
+             {"w/ under", "pump_one", QValidator::Acceptable},
+             {"w/ leading under", "_pump", QValidator::Invalid},
+             {"w/ trailing under", "pump_", QValidator::Acceptable},
+             {"w/ number", "pump1", QValidator::Acceptable},
+             {"w/ leading number", "1pump", QValidator::Invalid},
+             {"w/ leading !", "!pump1", QValidator::Invalid},
+             {"w/ leading ~", "~pump1", QValidator::Invalid},
+             {"w/ trailing ~", "pump1~", QValidator::Invalid},
+             {"w/ ~ separator", "pump~one", QValidator::Invalid},
+             {"w/ double hyphen", "pump--one", QValidator::Invalid},
+             {"w/ dunder", "pump__one", QValidator::Acceptable},
+             {"hyphen/under", "pump-_one", QValidator::Acceptable},
+             {"under/hyphen", "pump_-one", QValidator::Acceptable},
+             {"dunder/dunder", "__pump__", QValidator::Invalid},
+             {"end dunder", "pump__", QValidator::Acceptable},
+             {"w/ hyphen num", "pump-1", QValidator::Acceptable},
+             {"w/ under num", "pump_1", QValidator::Acceptable},
+             {"capital", "PUMP", QValidator::Acceptable},
+             {"lower", "pump", QValidator::Acceptable},
+             {"mixed", "PumpOne", QValidator::Acceptable},
+             {"camel", "pumpOne", QValidator::Acceptable},
+             {"non-English", u8"Помпа", QValidator::Acceptable}});
     }
     void testName() { test(scram::gui::Validator::name()); }
 
     void testPercent_data()
     {
-        QTest::addColumn<QString>("value");
-        QTest::addColumn<QValidator::State>("state");
-
-#if QT_VERSION >= 0x050500 ///< @todo Upgrade Qt.
-        QTest::newRow("empty") << "" << QValidator::Intermediate;
-#endif
-        QTest::newRow("valid") << "5" << QValidator::Acceptable;
-        QTest::newRow("zero") << "0" << QValidator::Invalid;
-        QTest::newRow("negative") << "-1" << QValidator::Invalid;
-        QTest::newRow("one") << "1" << QValidator::Acceptable;
-        QTest::newRow("hundred") << "100" << QValidator::Acceptable;
-        QTest::newRow("large") << "1010" << QValidator::Acceptable;
-        QTest::newRow("positive exponent") << "1e6" << QValidator::Invalid;
-        QTest::newRow("negative exponent") << "1e-6" << QValidator::Invalid;
-        QTest::newRow("double") << "0.1" << QValidator::Invalid;
-        QTest::newRow("bare dot") << ".1" << QValidator::Invalid;
-        QTest::newRow("string") << "one" << QValidator::Invalid;
-        QTest::newRow("expression") << "4*2" << QValidator::Invalid;
-        QTest::newRow("whitespace") << " 10" << QValidator::Invalid;
-        QTest::newRow("percent sign") << "1%" << QValidator::Acceptable;
-        QTest::newRow("bare percent") << "%" << QValidator::Invalid;
-        QTest::newRow("space percent") << "1 %" << QValidator::Invalid;
-        QTest::newRow("leading percent") << "%1" << QValidator::Invalid;
+        populateData<QString, QValidator::State>(
+            {"value", "state"},
+            {{"valid", "5", QValidator::Acceptable},
+             {"zero", "0", QValidator::Invalid},
+             {"negative", "-1", QValidator::Invalid},
+             {"one", "1", QValidator::Acceptable},
+             {"hundred", "100", QValidator::Acceptable},
+             {"large", "1010", QValidator::Acceptable},
+             {"positive exponent", "1e6", QValidator::Invalid},
+             {"negative exponent", "1e-6", QValidator::Invalid},
+             {"double", "0.1", QValidator::Invalid},
+             {"bare dot", ".1", QValidator::Invalid},
+             {"string", "one", QValidator::Invalid},
+             {"expression", "4*2", QValidator::Invalid},
+             {"whitespace", " 10", QValidator::Invalid},
+             {"percent sign", "1%", QValidator::Acceptable},
+             {"bare percent", "%", QValidator::Invalid},
+             {"space percent", "1 %", QValidator::Invalid},
+             {"leading percent", "%1", QValidator::Invalid}});
     }
     void testPercent() { test(scram::gui::Validator::percent()); }
 
     void testProbability_data()
     {
-        QTest::addColumn<QString>("value");
-        QTest::addColumn<QValidator::State>("state");
-
-        QTest::newRow("empty") << "" << QValidator::Intermediate;
-        QTest::newRow("valid") << "0.5" << QValidator::Acceptable;
-        QTest::newRow("zero") << "0" << QValidator::Acceptable;
-        QTest::newRow("negative") << "-1" << QValidator::Invalid;
-        QTest::newRow("one") << "1" << QValidator::Acceptable;
-        QTest::newRow("outside range") << "1.1" << QValidator::Intermediate;
-        QTest::newRow("positive exponent") << "1e6" << QValidator::Intermediate;
-        QTest::newRow("negative exponent") << "1e-6" << QValidator::Acceptable;
-        QTest::newRow("bare exponent") << "e-6" << QValidator::Intermediate;
-        QTest::newRow("bare dot") << ".1" << QValidator::Acceptable;
-        QTest::newRow("bare dot exponent") << ".1e-6" << QValidator::Acceptable;
-        QTest::newRow("string") << "one" << QValidator::Invalid;
-        QTest::newRow("expression") << "0.4*2" << QValidator::Invalid;
-        QTest::newRow("whitespace") << " 10" << QValidator::Invalid;
+        populateData<QString, QValidator::State>(
+            {"value", "state"},
+            {{"empty", "", QValidator::Intermediate},
+             {"valid", "0.5", QValidator::Acceptable},
+             {"zero", "0", QValidator::Acceptable},
+             {"negative", "-1", QValidator::Invalid},
+             {"one", "1", QValidator::Acceptable},
+             {"outside range", "1.1", QValidator::Intermediate},
+             {"positive exponent", "1e6", QValidator::Intermediate},
+             {"negative exponent", "1e-6", QValidator::Acceptable},
+             {"bare exponent", "e-6", QValidator::Intermediate},
+             {"bare dot", ".1", QValidator::Acceptable},
+             {"bare dot exponent", ".1e-6", QValidator::Acceptable},
+             {"string", "one", QValidator::Invalid},
+             {"expression", "0.4*2", QValidator::Invalid},
+             {"whitespace", " 10", QValidator::Invalid}});
     }
     void testProbability() { test(scram::gui::Validator::probability()); }
 
     void testNonNegative_data()
     {
-        QTest::addColumn<QString>("value");
-        QTest::addColumn<QValidator::State>("state");
-
-        QTest::newRow("empty") << "" << QValidator::Intermediate;
-        QTest::newRow("zero") << "0" << QValidator::Acceptable;
-        QTest::newRow("negative") << "-1" << QValidator::Invalid;
-        QTest::newRow("double") << "1.2" << QValidator::Acceptable;
-        QTest::newRow("positive exponent") << "1e6" << QValidator::Acceptable;
-        QTest::newRow("negative exponent") << "1e-6" << QValidator::Acceptable;
-        QTest::newRow("bare exponent") << "e-6" << QValidator::Intermediate;
-        QTest::newRow("bare dot") << ".1" << QValidator::Acceptable;
-        QTest::newRow("bare dot exponent") << ".1e6" << QValidator::Acceptable;
-        QTest::newRow("string") << "one" << QValidator::Invalid;
-        QTest::newRow("expression") << "4*2" << QValidator::Invalid;
-        QTest::newRow("whitespace") << " 10" << QValidator::Invalid;
+        populateData<QString, QValidator::State>(
+            {"value", "state"},
+            {{"empty", "", QValidator::Intermediate},
+             {"zero", "0", QValidator::Acceptable},
+             {"negative", "-1", QValidator::Invalid},
+             {"double", "1.2", QValidator::Acceptable},
+             {"positive exponent", "1e6", QValidator::Acceptable},
+             {"negative exponent", "1e-6", QValidator::Acceptable},
+             {"bare exponent", "e-6", QValidator::Intermediate},
+             {"bare dot", ".1", QValidator::Acceptable},
+             {"bare dot exponent", ".1e6", QValidator::Acceptable},
+             {"string", "one", QValidator::Invalid},
+             {"expression", "4*2", QValidator::Invalid},
+             {"whitespace", " 10", QValidator::Invalid}});
     }
     void testNonNegative() { test(scram::gui::Validator::nonNegative()); }
 
