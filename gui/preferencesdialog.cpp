d49bdaa69191187b17ecf8b37317655dce934f63
rakhimov <ol.rakhimov@gmail.com>
2017-12-23 12:38:14 -0800

    GUI: Add Polish into interface languages

--- gui/preferencesdialog.cpp
+++ gui/preferencesdialog.cpp
@@ -31,7 +31,7 @@ namespace scram {
 namespace gui {
 
 const char *const PreferencesDialog::m_languageToLocale[] = {
-    "en", "ru_RU", "de_DE", "es_ES", "tr_TR", "nl_NL", "id_ID"};
+    "en", "ru_RU", "de_DE", "es_ES", "tr_TR", "nl_NL", "id_ID", "pl_PL"};
 
 PreferencesDialog::PreferencesDialog(QSettings *preferences,
                                      QUndoStack *undoStack,
