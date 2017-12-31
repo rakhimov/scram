eb0dcd6aca6c18a6c915d9959a6c4d32fa3dba4a
rakhimov <ol.rakhimov@gmail.com>
2017-12-30 19:10:45 -0800

    Use relative __FILE__ path in error data
    With CMake, __FILE__ is absolute by default.
    For better messages, relative path is constructed from the __FILE__
    with macros and PROJECT_SOURCE_DIR definition.

--- src/error.h
+++ src/error.h
@@ -28,6 +28,15 @@
 #include <boost/exception/exception.hpp>
 #include <boost/exception/info.hpp>
 
+/// Check if CMake provides required definitions.
+#ifndef PROJECT_SOURCE_DIR
+#error "The project source directory is not provided w/ CMake."
+#endif
+
+/// The current file path relative to the project source directory.
+/// With CMake, the default __FILE__ is absolute.
+#define FILEPATH (__FILE__ + sizeof(PROJECT_SOURCE_DIR))
+
 /// Convenience macro to throw SCRAM exceptions.
 /// This is similar to BOOST_THROW_EXCEPTION;
 /// however, it doesn't obfuscate
@@ -36,7 +45,7 @@
 /// @param[in] err  The error type deriving from boost::exception.
 #define SCRAM_THROW(err)                                                       \
   throw err << ::boost::throw_function(BOOST_THROW_EXCEPTION_CURRENT_FUNCTION) \
-            << ::boost::throw_file(__FILE__) << ::boost::throw_line(__LINE__)
+            << ::boost::throw_file(FILEPATH) << ::boost::throw_line(__LINE__)
 
 namespace scram {
 
