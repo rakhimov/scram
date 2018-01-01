d088aaf2e89a6a21aa3325ebcf71658db159e093
rakhimov <ol.rakhimov@gmail.com>
2017-12-31 23:00:51 -0800

    Separate FILE_REL_PATH into ext/source_info.h
    This is used in both the core and GUI
    to provide the source file paths in the error and assertion messages.

--- gui/guiassert.h
+++ gui/guiassert.h
@@ -24,6 +24,8 @@
 #include <QMessageBox>
 #include <QString>
 
+#include "src/ext/source_info.h"
+
 /// Assertion that avoids crashing the application.
 /// To simulate the standard assert (i.e., crash on error),
 /// define the environmental variable QT_FATAL_CRITICALS to non-empty value.
@@ -34,12 +36,12 @@
     do {                                                                       \
         if (cond)                                                              \
             break;                                                             \
-        qCritical("Assertion failure: %s in %s line %d", #cond, __FILE__,      \
+        qCritical("Assertion failure: %s in %s line %d", #cond, FILE_REL_PATH, \
                   __LINE__);                                                   \
         QMessageBox::critical(nullptr, QStringLiteral("Assertion Failure"),    \
                               QStringLiteral("%1 in %2 line %3")               \
                                   .arg(QStringLiteral(#cond),                  \
-                                       QString::fromUtf8(__FILE__),            \
+                                       QString::fromUtf8(FILE_REL_PATH),       \
                                        QString::number(__LINE__)));            \
         return ret;                                                            \
     } while (false)
---
+++ src/ext/source_info.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 Olzhas Rakhimov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// @file
+/// Helper facilities to get source file information.
+
+#ifndef SCRAM_SRC_EXT_SOURCE_INFO_H_
+#define SCRAM_SRC_EXT_SOURCE_INFO_H_
+
+/// Check if CMake provides required definitions.
+#ifndef PROJECT_SOURCE_DIR
+#error "The project source directory is not provided w/ CMake."
+#endif
+
+/// The current file path relative to the project source directory.
+/// With CMake, the default __FILE__ is absolute.
+#define FILE_REL_PATH (__FILE__ + sizeof(PROJECT_SOURCE_DIR))
+
+#endif  // SCRAM_SRC_EXT_SOURCE_INFO_H_
