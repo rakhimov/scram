8ea34f9d5e90f748c1ae8dae065ed0f9ce6c52c3
rakhimov <ol.rakhimov@gmail.com>
2018-02-15 20:04:58 -0800

    Update doxygen config for CI [skip appveyor]

--- .travis/lint.sh
+++ .travis/lint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -ev
+set -o pipefail
 
 CLANG_FORMAT="./run-clang-format/run-clang-format.py --clang-format-executable clang-format-${CLANG_VERSION}"
 
@@ -9,14 +10,9 @@ CLANG_FORMAT="./run-clang-format/run-clang-format.py --clang-format-executable c
 # ${CLANG_FORMAT} tests/*.{h,cc}
 
 # Check documentation coverage
-which doxygen || exit 1
-doxygen ./.travis/doxygen.conf > /dev/null 2> doc_errors.txt || (cat doc_errors.txt && exit 1)
-doxygen ./.travis/gui_doxygen.conf > /dev/null 2>> doc_errors.txt || (cat doc_errors.txt && exit 1)
-if [[ -s doc_errors.txt ]]; then
-  echo "Documentation errors:" >&2
-  cat doc_errors.txt >&2
-  exit 1
-fi
+doxygen ./.travis/doxygen.conf
+doxygen ./.travis/gui_doxygen.conf
+
 ls src/**/*.{cc,h} | xargs -n 1 grep -q '^/// @file$'  # Missing file doc.
 ls gui/*.{cpp,h} | xargs -n 1 grep -q '^/// @file$'  # Missing file doc.
 
@@ -32,12 +28,3 @@ cpplint --quiet tests/*.{cc,h,cc.in}
 # Python linting
 yapf -d scripts/*.py scripts/test/*.py tests/*.py tests/input/fta/run_inputs.py
 prospector scripts/*.py
-
-# TODO: Fix lupdate for C++17
-# Qt lupdate warnings
-# lupdate gui -ts gui/translations/scramgui_en.ts 2> ts_warnings.txt || (cat ts_warnings.txt && exit 1)
-# if [[ -s ts_warnings.txt ]]; then
-#   echo "Qt Lupdate warnings:" >&2
-#   cat ts_warnings.txt >&2
-#   exit 1
-# fi
