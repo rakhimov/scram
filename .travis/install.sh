0a5a319a92d64a9131db2c0b53866764afd9d1b0
rakhimov <ol.rakhimov@gmail.com>
2018-05-24 20:47:56 -0700

    Switch to Python 3 on Travis CI [skip appveyor]
    This is achieved with virtualenv for now.
    
    Issue #272

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,22 +10,22 @@ if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
   brew install qt5
   brew install ccache
   # brew install python --universal
-  sudo pip2 install pytest
+  pip install pytest
 fi
 
 [[ "${TRAVIS_OS_NAME}" == "linux" ]] || exit 0
 
-pip install --user -U pip wheel
+pip install -U pip wheel
 
 if [[ "${CONFIG}" == "Lint" ]]; then
   git clone https://github.com/Sarcasm/run-clang-format
-  pip install --user -r requirements-dev.txt  # Python linting tools.
+  pip install -r requirements-dev.txt  # Python linting tools.
   exit 0  # Compilation dependencies are not required.
 fi
 
-pip install --user pytest  # Testing main() requires pytest!
+pip install pytest  # Testing main() requires pytest!
 
 if [[ "${CONFIG}" == "Coverage" ]]; then
-  pip install --user -r requirements-dev.txt
-  pip install --user -r requirements-tests.txt
+  pip install -r requirements-dev.txt
+  pip install -r requirements-tests.txt
 fi
